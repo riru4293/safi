2b365fd46d3a72dd66a964394d424ccad95fbba6
riru <riru4293@gmail.com>
2024-01-06 02:07:09 +0900

    Created a temporary FooterContext.

--- src/main/java/trial/PersistenceResource.java
+++ src/main/java/trial/PersistenceResource.java
@@ -3,11 +3,17 @@ package trial;
 import jakarta.inject.Inject;
 import jakarta.persistence.EntityManager;
 import jakarta.transaction.Transactional;
+import jakarta.validation.Validator;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.core.Response;
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
+import java.util.Map;
+import jp.mydns.projectk.safi.constant.AttName;
 import jp.mydns.projectk.safi.entity.UserEntity;
 import jp.mydns.projectk.safi.producer.EntityManagerProducer.ForBatch;
+import jp.mydns.projectk.safi.value.ValidityPeriod;
 
 @Path("persistence")
 public class PersistenceResource {
@@ -15,6 +21,9 @@ public class PersistenceResource {
     @Inject
     @ForBatch
     private EntityManager em;
+    
+    @Inject
+    private Validator validator;
 
     @GET
     @Path("c")
@@ -24,10 +33,16 @@ public class PersistenceResource {
         var u = new UserEntity();
 
         u.setId("id");
-        u.setName("taro");
         u.setEnabled(true);
+        u.setName("taro");
+        u.setAtts(Map.of(AttName.ATT01, "a1",AttName.ATT02, "a2",
+                AttName.ATT03, "a3",AttName.ATT04, "a4",AttName.ATT05, "a5",
+                AttName.ATT06, "a6",AttName.ATT07, "a7",AttName.ATT08, "a8",
+                AttName.ATT09, "a9",AttName.ATT10, "aa"));
+        u.setValidityPeriod(new ValidityPeriod.Builder().build(validator));
         u.setDigest("x");
         u.setNote("Trial.");
+        u.setVersion(0);
 
         em.persist(u);
 
