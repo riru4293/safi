ac244b8baf6440e815590e94a538c5ba561f426e
riru <riru4293@gmail.com>
2024-01-15 07:33:27 +0900

    Add UncheckedInterruptedException class.

---
+++ src/main/java/jp/mydns/projectk/safi/batch/UncheckedInterruptedException.java
@@ -0,0 +1,50 @@
+/*
+ * Copyright (c) 2024, Project-K
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * * Redistributions of source code must retain the above copyright notice, this
+ *   list of conditions and the following disclaimer.
+ * * Redistributions in binary form must reproduce the above copyright notice,
+ *   this list of conditions and the following disclaimer in the documentation
+ *   and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+package jp.mydns.projectk.safi.batch;
+
+/**
+ * {@code UncheckedInterruptedException} is the exception thrown when current thread was interrupted. It is intended to
+ * be used in processes where checked exceptions are not suitable, such as {@code java.​util.​stream.​Stream}.
+ *
+ * @author riru
+ * @version 1.0.0
+ * @since 1.0.0
+ */
+public class UncheckedInterruptedException extends RuntimeException {
+
+    private static final long serialVersionUID = -3141128093589066633L;
+
+    /**
+     * Constructor.
+     *
+     * @param cause the cause (which is saved for later retrieval by the {@link #getCause()} method). (A {@code null}
+     * value is permitted, and indicates that the cause is nonexistent or unknown.)
+     * @since 1.0.0
+     */
+    public UncheckedInterruptedException(InterruptedException cause) {
+        super(cause);
+    }
+}
