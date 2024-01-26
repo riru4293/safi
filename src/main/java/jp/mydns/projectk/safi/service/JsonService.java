f863192c47adb8e909f504ed598dd7baad6a1b11
riru <riru4293@gmail.com>
2024-01-26 13:56:16 +0900

    Optimize

---
+++ src/main/java/jp/mydns/projectk/safi/service/JsonService.java
@@ -0,0 +1,154 @@
+/*
+ * Copyright (c) 2024, Project-K
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * * Redistributions of source code must retain the above copyright notice, this
+ *   list of conditions and the following disclaimer.
+ * * Redistributions in binary form must reproduce the above copyright notice,
+ *   this list of conditions and the following disclaimer in the documentation
+ *   and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+package jp.mydns.projectk.safi.service;
+
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
+import jakarta.json.JsonObject;
+import jakarta.json.JsonValue;
+import jakarta.json.bind.Jsonb;
+import jakarta.json.bind.JsonbException;
+import java.util.Map;
+import java.util.Objects;
+import static jp.mydns.projectk.safi.util.EntryUtils.compute;
+import jp.mydns.projectk.safi.util.JsonUtils;
+import static jp.mydns.projectk.safi.util.LambdaUtils.toLinkedHashMap;
+import jp.mydns.projectk.safi.value.JsonObjectVo;
+
+/**
+ * Utilities for <i>JSON</i>.
+ *
+ * @author riru
+ * @version 1.0.0
+ * @since 1.0.0
+ */
+@ApplicationScoped
+public class JsonService {
+
+    @Inject
+    private Jsonb jsonb;
+
+    /**
+     * Conversion to the {@code Map<String, String>}.
+     *
+     * @param obj object that convertible to {@code JsonObject}
+     * @return the value converted to {@code Map<String, String>}
+     * @throws NullPointerException if {@code obj} is {@code null}
+     * @throws ClassCastException if {@code obj} is {@code JsonValue} but not {@code JsonObject}
+     * @throws JsonbException if any unexpected error(s) occur(s) during conversion.
+     * @since 1.0.0
+     */
+    public Map<String, String> toStringMap(Object obj) {
+        return toJsonObject(Objects.requireNonNull(obj)).entrySet().stream()
+                .map(compute(JsonUtils::toString)).collect(toLinkedHashMap());
+    }
+
+    /**
+     * Conversion to {@code JsonObject}. It is wrapper method of the
+     * {@link JsonbUtils#toJsonObject(java.lang.Object, jakarta.json.bind.Jsonb)}.
+     *
+     * @param obj source value
+     * @return the value converted to the {@code JsonObject}
+     * @throws NullPointerException if {@code obj} is {@code null}
+     * @throws ClassCastException if {@code obj} is {@code JsonValue} but not {@code JsonObject}
+     * @throws JsonbException if any unexpected error(s) occur(s) during conversion.
+     * @since 1.0.0
+     */
+    public JsonObject toJsonObject(Object obj) {
+        return Objects.requireNonNull(obj) instanceof JsonValue jv ? jv.asJsonObject() : JsonUtils.toJsonObject(obj, jsonb);
+    }
+
+    /**
+     * Merge two {@code JsonObject}. It is wrapper method of the
+     * {@link JsonUtils#merge(jakarta.json.JsonObject, jakarta.json.JsonObject)}.
+     *
+     * @param base base value
+     * @param ow overwrite value
+     * @return merged value
+     * @throws NullPointerException if any argument is {@code null}
+     * @since 1.0.0
+     */
+    public JsonObject merge(JsonObject base, JsonObject ow) {
+        return JsonUtils.merge(Objects.requireNonNull(base), Objects.requireNonNull(ow));
+    }
+
+    /**
+     * Returns a {@code JsonObjectVo} representation of the {@code obj}.
+     *
+     * @param jsonObj the {@code JsonObject}
+     * @return {@code JsonObjectVo} representation of the {@code jsonObj}
+     * @throws NullPointerException if {@code jsonObj} is {@code null}
+     * @since 1.0.0
+     */
+    public JsonObjectVo toJsonObjectVo(JsonObject jsonObj) {
+        return new JsonObjectVo(Objects.requireNonNull(jsonObj));
+    }
+
+    /**
+     * Conversion to {@code JsonObjectVo}.
+     *
+     * @param obj source value
+     * @return the value converted to the {@code JsonObjectVo}
+     * @throws NullPointerException if {@code obj} is {@code null}
+     * @throws JsonbException if any unexpected error(s) occur(s) during conversion.
+     * @see #toJsonObject(java.lang.Object)
+     * @see #toJsonObjectVo(jakarta.json.JsonObject)
+     * @since 1.0.0
+     */
+    public JsonObjectVo toJsonObjectVo(Object obj) {
+        return toJsonObjectVo(toJsonObject(Objects.requireNonNull(obj)));
+    }
+
+    /**
+     * Deserialize to {@code T} from JSON.
+     *
+     * @param <T> destination type. It should support deserialization from JSON.
+     * @param json source value
+     * @param clazz destination type
+     * @return the value deserialized to the {@code T}
+     * @throws NullPointerException if any argument is {@code null}
+     * @throws JsonbException if any unexpected error(s) occur(s) during conversion.
+     * @since 1.0.0
+     */
+    public <T> T fromJsonObject(JsonObject json, Class<T> clazz) {
+        return jsonb.fromJson(Objects.requireNonNull(json).toString(), Objects.requireNonNull(clazz));
+    }
+
+    /**
+     * Convert to type of {@code T} via JSON.
+     *
+     * @param <T> destination type. It should support deserialization from JSON.
+     * @param obj source value. It should support serialization to JSON.
+     * @param clazz destination type
+     * @return the value converted to the {@code T}
+     * @throws NullPointerException if any argument is {@code null}
+     * @throws JsonbException if any unexpected error(s) occur(s) during conversion.
+     * @since 1.0.0
+     */
+    public <T> T convertViaJson(Object obj, Class<T> clazz) {
+        return jsonb.fromJson(toJsonObject(Objects.requireNonNull(obj)).toString(), Objects.requireNonNull(clazz));
+    }
+}
