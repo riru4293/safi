864d9d61cb84127da77655698842749dbd22c71b
riru <riru4293@gmail.com>
2024-02-16 07:25:11 +0900

    Optimized

--- src/main/java/jp/mydns/projectk/safi/dxo/ImportationDxo.java
+++ src/main/java/jp/mydns/projectk/safi/dxo/ImportationDxo.java
@@ -25,12 +25,29 @@
  */
 package jp.mydns.projectk.safi.dxo;
 
+import jakarta.inject.Inject;
 import jakarta.validation.ConstraintViolationException;
+import jakarta.validation.Validator;
+import java.util.Collections;
+import java.util.EnumMap;
+import java.util.Map;
 import java.util.Objects;
+import java.util.Optional;
+import static java.util.function.Function.identity;
+import static java.util.stream.Collectors.collectingAndThen;
+import static java.util.stream.Collectors.toMap;
+import java.util.stream.Stream;
+import jp.mydns.projectk.safi.constant.AttKey;
 import jp.mydns.projectk.safi.entity.ContentEntity;
 import jp.mydns.projectk.safi.entity.ImportationWorkEntity;
+import static jp.mydns.projectk.safi.util.LambdaUtils.alwaysThrow;
+import static jp.mydns.projectk.safi.util.LambdaUtils.f;
+import static jp.mydns.projectk.safi.util.LambdaUtils.p;
+import jp.mydns.projectk.safi.util.TimeUtils;
+import jp.mydns.projectk.safi.validator.Unsafe;
 import jp.mydns.projectk.safi.value.ContentValue;
 import jp.mydns.projectk.safi.value.TransResult;
+import jp.mydns.projectk.safi.value.ValidityPeriod;
 import trial.ImportationValue;
 
 /**
@@ -46,7 +63,7 @@ import trial.ImportationValue;
 public interface ImportationDxo<E extends ContentEntity, V extends ContentValue<V>> {
 
     /**
-     * Exchange to working entity from importation value.
+     * Build a working entity from importation value.
      *
      * @param importValue the {@code ImportationValue}
      * @return the {@code ImportationWorkEntity}
@@ -54,15 +71,7 @@ public interface ImportationDxo<E extends ContentEntity, V extends ContentValue<
      * @throws IllegalArgumentException if {@code importValue} represents an explicit deletion
      * @since 1.0.0
      */
-    default ImportationWorkEntity toImportationWorkEntity(ImportationValue<V> importValue) {
-        Objects.requireNonNull(importValue);
-
-        var e = new ImportationWorkEntity();
-        e.setId(importValue.getId());
-        e.setDigest(importValue.getContent().getDigest());
-
-        return e;
-    }
+    ImportationWorkEntity toImportationWorkEntity(ImportationValue<V> importValue);
 
     /**
      * Build an importation value from the transform result.
@@ -103,4 +112,66 @@ public interface ImportationDxo<E extends ContentEntity, V extends ContentValue<
      * @since 1.0.0
      */
     E toEntity(ImportationValue<V> value);
+
+    abstract class AbstractImportationDxo<E extends ContentEntity, V extends ContentValue<V>>
+            implements ImportationDxo<E, V> {
+
+        @Inject
+        private Validator validator;
+
+        /**
+         * {@inheritDoc}
+         *
+         * @throws NullPointerException if {@code importValue} is {@code null}
+         * @throws IllegalArgumentException if {@code importValue} represents an explicit deletion
+         * @since 1.0.0
+         */
+        @Override
+        public ImportationWorkEntity toImportationWorkEntity(ImportationValue<V> importValue) {
+            Objects.requireNonNull(importValue);
+
+            var e = new ImportationWorkEntity();
+            e.setId(importValue.getId());
+            e.setDigest(importValue.getContent().getDigest());
+
+            return e;
+        }
+
+        /**
+         * Build the {@code ValidityPeriod} from key-value content. Use key values ​​from {@code from}, {@code to},
+         * {@code ban}. Use the default value if not exists a value in key-value content.
+         *
+         * @param value key-value content
+         * @return the {@code ValidityPeriod}. Please note, it has not been verified.
+         * @throws NullPointerException if {@code content} is {@code null}
+         * @see ValidityPeriod#defaultFrom()
+         * @see ValidityPeriod#defaultTo()
+         * @see ValidityPeriod#defaultBan()
+         * @since 1.0.0
+         */
+        protected ValidityPeriod toValidityPeriod(Map<String, String> value) {
+            return new ValidityPeriod.Builder()
+                    .withFrom(TimeUtils.tryToLocalDateTime(
+                            value.get("from")).map(TimeUtils::toOffsetDateTime).orElseGet(ValidityPeriod::defaultFrom))
+                    .withTo(TimeUtils.tryToLocalDateTime(
+                            value.get("to")).map(TimeUtils::toOffsetDateTime).orElseGet(ValidityPeriod::defaultFrom))
+                    .withBan(Optional.ofNullable(
+                            value.get("ban")).map(Boolean::valueOf).orElseGet(ValidityPeriod::defaultBan))
+                    .build(validator, Unsafe.class);
+        }
+
+        /**
+         * Build the attributes from key-value content. Use key values ​​from the {@link AttKey#toString()}.
+         *
+         * @param value key-value content
+         * @return attributes
+         * @throws NullPointerException if {@code content} is {@code null}
+         * @since 1.0.0
+         */
+        protected Map<AttKey, String> toAtts(Map<String, String> value) {
+            return Stream.of(AttKey.values()).filter(p(value::containsKey, AttKey::toString))
+                    .collect(collectingAndThen(toMap(identity(), f(value::get).compose(AttKey::toString),
+                            alwaysThrow(), () -> new EnumMap<>(AttKey.class)), Collections::unmodifiableMap));
+        }
+    }
 }
