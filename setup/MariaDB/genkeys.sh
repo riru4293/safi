1468a3b0e2d438f9d97bba16ec7b27c91754d590
riru <riru4293@gmail.com>
2024-12-02 18:26:22 +0900

    MariaDB installation guide for linux

--- setup/MariaDB/genkeys.sh
+++ setup/MariaDB/genkeys.sh
@@ -5,7 +5,7 @@ DEST="/tmp/MariaDB-Keys.$(date +%F)"
 if [ ! -d "${DEST}" ]; then mkdir "${DEST}"; fi
 for i in 1 2 3 4 5 6 7 8 9; do echo $i';'$(openssl rand -hex 32); done | tee "${DEST}/tdekeys.txt"
 openssl rand -hex 128 > "${DEST}/tdekeys.key"
-openssl enc -aes-256-cbc -pbkdf2 -iter 91342 -md sha1 \
+openssl enc -aes-256-cbc -md sha1 \
   -pass file:$(readlink -f "${DEST}/tdekeys.key") \
   -in "${DEST}/tdekeys.txt" -out "${DEST}/tdekeys.enc"
 rm "${DEST}/tdekeys.txt"
