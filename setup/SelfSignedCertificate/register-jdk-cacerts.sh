66359a7aee19215a20a7b9ca00e1827860fd344c
riru <riru4293@gmail.com>
2025-01-10 23:33:42 +0900

    Update register-jdk-cacerts.sh

--- setup/SelfSignedCertificate/register-jdk-cacerts.sh
+++ setup/SelfSignedCertificate/register-jdk-cacerts.sh
@@ -1,1 +1,15 @@
 #!/bin/sh
+
+ORG='project-k'
+CA_HOME="${HOME}/.local/CA"
+CA_CERT_NAME="ca.${ORG}"
+JDK_PREFIX="${HOME}/.local/Java"
+
+# Register CA to JDK
+for jhome in $( \find "${JDK_PREFIX}" -maxdepth 1 -name "jdk-*" -type d ); do
+  echo $(basename "${jhome}")
+  ${jhome}/bin/keytool -delete -cacerts -alias "${ORG}" -storepass 'changeit'
+  ${jhome}/bin/keytool -import -cacerts -alias "${ORG}" -storepass 'changeit' -noprompt \
+    -file "${CA_HOME}/certs/${CA_CERT_NAME}.crt"
+  ${jhome}/bin/keytool -list -cacerts -alias "${ORG}" -v -storepass 'changeit'
+done
