ce796370eec73c18e3af92794a49ad316338d40a
riru <riru4293@gmail.com>
2025-01-11 22:29:42 +0900

    Update register-jdk-cacerts.ps1

--- setup/SelfSignedCertificate/register-jdk-cacerts.ps1
+++ setup/SelfSignedCertificate/register-jdk-cacerts.ps1
@@ -1,71 +1,88 @@
 $ErrorActionPreference = 'Continue'
 
-$ORG = 'project-k'
-$env:CA_HOME = "${env:LOCALAPPDATA}\CA"
-$CA_CERT_NAME = "ca.${ORG}"
+${ORG} = 'project-k'
+${CA_CERT} = "${env:LOCALAPPDATA}\CA\certs\ca.${ORG}.crt"
+${JDKS} = @(
+  @{ HOME = "${env:LOCALAPPDATA}\Programs\Java\jdk8" ; STORE_PASS = 'changeit' }
+  @{ HOME = "${env:LOCALAPPDATA}\Programs\Java\jdk11"; STORE_PASS = 'changeit' }
+  @{ HOME = "${env:LOCALAPPDATA}\Programs\Java\jdk17"; STORE_PASS = 'changeit' }
+  @{ HOME = "${env:LOCALAPPDATA}\Programs\Java\jdk21"; STORE_PASS = 'changeit' }
+  @{ HOME = "${env:LOCALAPPDATA}\Programs\Java\jdk23"; STORE_PASS = 'changeit' }
+)
 
-# Register CA to JDK
-$JDK_PREFIX = "${env:LOCALAPPDATA}\Programs\Java"
-$JdkNames = Get-ChildItem "${JDK_PREFIX}" | Where-Object { $_.PSIsContainer } | Foreach-Object { $_.Name } `
-| Out-String -Stream | Select-String "jdk-"
+foreach ( ${jdk} in ${JDKS} ) {
+  ${h} = ${jdk}.HOME
+  ${p} = ${jdk}.STORE_PASS
 
-$ErrorActionPreference = 'Continue'
-foreach ( ${JdkName} in ${JdkNames} ) {
-  Write-Output ${JdkName}
+  if( Test-Path "${h}\bin\java.exe" ) {
+    Write-Host "Found JDK: ${h}"
 
-  $JAVA_HOME = Join-Path -Path "${JDK_PREFIX}" -ChildPath "${JdkName}"
+    # Get Java version outputs
+    ${a} = ( & "${h}\bin\java.exe" -version 2>&1 )
 
-  Start-Process -Wait -NoNewWindow -FilePath "${JAVA_HOME}\bin\keytool.exe" -ArgumentList @(
-    '-delete', '-cacerts', '-alias', ${ORG}, '-storepass', 'changeit'
-  )
-
-  Start-Process -Wait -NoNewWindow -FilePath "${JAVA_HOME}\bin\keytool.exe" -ArgumentList @(
-    '-import', '-cacerts', '-alias', ${ORG}, '-storepass', 'changeit', '-noprompt' `
-  , '-file', "`"${env:CA_HOME}\certs\${CA_CERT_NAME}.crt`""
-  )
-
-  Start-Process -Wait -NoNewWindow -FilePath "${JAVA_HOME}\bin\keytool.exe" -ArgumentList @(
-    '-list', '-cacerts', '-alias', ${ORG}, '-v'
-  )
+    ${a} | ForEach-Object {
+      if ( $_ -match 'version "([\d\.]+)' ) {
+        ${v} = $Matches[1]
+        Write-Host "Java version: ${v}"
+  
+        # Major version
+        ${m} = if ( ${v} -like '1.*' ) {
+            # Version 8 and under (example: 1.8 → 8)
+            ${v}.Split('.')[1]
+        } else {
+            # Newer than version 8 (example: 9.0 -> 9)
+            ${v}.Split('.')[0]
+        }
+        
+        # Register the CA certificate
+        if ( [int]${m} -le 8 ) {
+          & "${h}\bin\keytool.exe" -delete -alias ${ORG} -storepass "${p}" -keystore "${h}\jre\lib\security\cacerts"
+          & "${h}\bin\keytool.exe" -import -alias ${ORG} -storepass "${p}" -keystore "${h}\jre\lib\security\cacerts" `
+          -noprompt -trustcacerts -file "${CA_CERT}"
+        } else {
+          & "${h}\bin\keytool.exe" -delete -cacerts -alias ${ORG} -storepass "${p}"
+          & "${h}\bin\keytool.exe" -import -cacerts -alias ${ORG} -storepass "${p}" -noprompt -file "${CA_CERT}"
+        }
+      }
+    }
+  }
 }
 
 Read-Host "Press enter to exit."
 
-
 # SIG # Begin signature block
-# MIIGgQYJKoZIhvcNAQcCoIIGcjCCBm4CAQExDzANBglghkgBZQMEAgEFADB5Bgor
-# BgEEAYI3AgEEoGswaTA0BgorBgEEAYI3AgEeMCYCAwEAAAQQH8w7YFlLCE63JNLG
-# KX7zUQIBAAIBAAIBAAIBAAIBADAxMA0GCWCGSAFlAwQCAQUABCCWGAmznSh3ALLc
-# s/ifzYYADcg0ENaDktjR/RFQeWZAQqCCA88wggPLMIICs6ADAgECAgEGMA0GCSqG
-# SIb3DQEBCwUAMFExCzAJBgNVBAYTAkpQMQ4wDAYDVQQIDAVPc2FrYTESMBAGA1UE
-# CgwJUHJvamVjdC1LMR4wHAYDVQQDDBVjYS5wcm9qZWN0LWsubXlkbnMuanAwHhcN
-# MjUwMTAyMTM0NzE3WhcNMjcwOTI5MTM0NzE3WjBjMQswCQYDVQQGEwJKUDEOMAwG
-# A1UECAwFT3Nha2ExEjAQBgNVBAoMCVByb2plY3QtSzENMAsGA1UEAwwEcmlydTEh
-# MB8GCSqGSIb3DQEJARYScmlydTQyOTNAZ21haWwuY29tMIIBIjANBgkqhkiG9w0B
-# AQEFAAOCAQ8AMIIBCgKCAQEA4hE8VczM9PPbWl8+NQizY8kHVZAYOvj50IKTePXs
-# QVArHelkk5stcG12MDBurFkmcX8OjJSmwJxUcvCoju3v6V+xSKrv8xNEUiEkG97m
-# gpEIYrd9gN7b5dLV9jnIWMmZaabx5EjuBIkeL0goIlHNndpnn0SbIjliF5+ey1jM
-# qHmuj8W0cXwmAk2LHmVZFB+yqjxNslws2DqdgaYG4qxN6CBWY1iLlJLXPrrbLr2R
-# rFf+FZqdAJPxuzs/AEp/E3CM5UMLtdV36UYHnV4J9R0s6qC+bk58qdjDyFvxhpz4
-# 0EfzaMkaRkP9x3Gi9P5gaiNdRRchaNfrv5THbn9NgqcUZQIDAQABo4GbMIGYMB0G
-# A1UdDgQWBBTW8lFnA3uEG/fpnwqSo7aodhgyIzAJBgNVHRMEAjAAMAsGA1UdDwQE
-# AwIHgDArBgNVHSUEJDAiBggrBgEFBQcDAwYKKwYBBAGCNwIBFQYKKwYBBAGCNwIB
-# FjARBglghkgBhvhCAQEEBAMCBLAwHwYDVR0jBBgwFoAUsAKpDwH3fVWDYfS0NspO
-# CdnXRuowDQYJKoZIhvcNAQELBQADggEBAGAEhZb4/w5iNCpALFq7OopmEAsY0BPq
-# bAhyxfs0iK0nt9gDWAfkl4MEIfgZtx6FHmxcDuvvnHCEYntFwfYu7WNAT9aMEJLU
-# iFlAQI/zM+cfbsRnWRhBllFqnfSMlTxeBh7It6KJ+dEfH65OmUT7OwyniFmWsFZV
-# 2dAFlHC7wVk89+r3AhiOF7exfyCDUwiVbmfDMHDEDq0CjxLv85awwoDaTDFHYZ5s
-# amDlxjIB+PIItv13hPt4zDMGdt7zvqEy/nfrB06d6UA3g+ZRI5FXYFpqFg4MtoD8
-# qjhbY+BC4vsIXlYzJHLaYzKiBPqXDzZky0wfooC71F88QIF9pjMoEm8xggIIMIIC
-# BAIBATBWMFExCzAJBgNVBAYTAkpQMQ4wDAYDVQQIDAVPc2FrYTESMBAGA1UECgwJ
-# UHJvamVjdC1LMR4wHAYDVQQDDBVjYS5wcm9qZWN0LWsubXlkbnMuanACAQYwDQYJ
-# YIZIAWUDBAIBBQCggYQwGAYKKwYBBAGCNwIBDDEKMAigAoAAoQKAADAZBgkqhkiG
-# 9w0BCQMxDAYKKwYBBAGCNwIBBDAcBgorBgEEAYI3AgELMQ4wDAYKKwYBBAGCNwIB
-# FjAvBgkqhkiG9w0BCQQxIgQgvJK+4XLsYjl2xxHxaMCNzUfB775dcgu1O0t3Iqnv
-# vcowDQYJKoZIhvcNAQEBBQAEggEASW287c6Twkz47gK9aygQZcbEW/vztKuWUqek
-# 00ec9uc6p8BX6H8Sz61wwavZLLgINrt0dmVCCfigwpUV5nb8LfnNFPHPLOojIWj/
-# 7/V6J6acAHgljza7vCUVR3xtG/rOghj+KWeqP3pse16l4Q+0C4Xx9BULB9ctuXzv
-# TSP3Btd9W03+zw9dFU5MIyR2xMsYy1pBnHgFl7Gz5C/egmMJF78B59h6bSXn4cJ/
-# fO2Zn8VmVjfUWKZwvXcWGTWTyZ8lkdm2f0GXSek/HaYgqckcE+dVa4U2P9vFiyvX
-# /XyTJ3XnzEDHr6tad+1nM1pK58KQWiY8NQHvSnFBL5BILsoohA==
+# MIIGXAYJKoZIhvcNAQcCoIIGTTCCBkkCAQExCzAJBgUrDgMCGgUAMGkGCisGAQQB
+# gjcCAQSgWzBZMDQGCisGAQQBgjcCAR4wJgIDAQAABBAfzDtgWUsITrck0sYpfvNR
+# AgEAAgEAAgEAAgEAAgEAMCEwCQYFKw4DAhoFAAQUJBHCW1oNN6JpYtZV1+981Pjg
+# VJqgggPPMIIDyzCCArOgAwIBAgIBBjANBgkqhkiG9w0BAQsFADBRMQswCQYDVQQG
+# EwJKUDEOMAwGA1UECAwFT3Nha2ExEjAQBgNVBAoMCVByb2plY3QtSzEeMBwGA1UE
+# AwwVY2EucHJvamVjdC1rLm15ZG5zLmpwMB4XDTI1MDEwMjEzNDcxN1oXDTI3MDky
+# OTEzNDcxN1owYzELMAkGA1UEBhMCSlAxDjAMBgNVBAgMBU9zYWthMRIwEAYDVQQK
+# DAlQcm9qZWN0LUsxDTALBgNVBAMMBHJpcnUxITAfBgkqhkiG9w0BCQEWEnJpcnU0
+# MjkzQGdtYWlsLmNvbTCCASIwDQYJKoZIhvcNAQEBBQADggEPADCCAQoCggEBAOIR
+# PFXMzPTz21pfPjUIs2PJB1WQGDr4+dCCk3j17EFQKx3pZJObLXBtdjAwbqxZJnF/
+# DoyUpsCcVHLwqI7t7+lfsUiq7/MTRFIhJBve5oKRCGK3fYDe2+XS1fY5yFjJmWmm
+# 8eRI7gSJHi9IKCJRzZ3aZ59EmyI5YhefnstYzKh5ro/FtHF8JgJNix5lWRQfsqo8
+# TbJcLNg6nYGmBuKsTeggVmNYi5SS1z662y69kaxX/hWanQCT8bs7PwBKfxNwjOVD
+# C7XVd+lGB51eCfUdLOqgvm5OfKnYw8hb8Yac+NBH82jJGkZD/cdxovT+YGojXUUX
+# IWjX67+Ux25/TYKnFGUCAwEAAaOBmzCBmDAdBgNVHQ4EFgQU1vJRZwN7hBv36Z8K
+# kqO2qHYYMiMwCQYDVR0TBAIwADALBgNVHQ8EBAMCB4AwKwYDVR0lBCQwIgYIKwYB
+# BQUHAwMGCisGAQQBgjcCARUGCisGAQQBgjcCARYwEQYJYIZIAYb4QgEBBAQDAgSw
+# MB8GA1UdIwQYMBaAFLACqQ8B931Vg2H0tDbKTgnZ10bqMA0GCSqGSIb3DQEBCwUA
+# A4IBAQBgBIWW+P8OYjQqQCxauzqKZhALGNAT6mwIcsX7NIitJ7fYA1gH5JeDBCH4
+# GbcehR5sXA7r75xwhGJ7RcH2Lu1jQE/WjBCS1IhZQECP8zPnH27EZ1kYQZZRap30
+# jJU8XgYeyLeiifnRHx+uTplE+zsMp4hZlrBWVdnQBZRwu8FZPPfq9wIYjhe3sX8g
+# g1MIlW5nwzBwxA6tAo8S7/OWsMKA2kwxR2GebGpg5cYyAfjyCLb9d4T7eMwzBnbe
+# 876hMv536wdOnelAN4PmUSORV2BaahYODLaA/Ko4W2PgQuL7CF5WMyRy2mMyogT6
+# lw82ZMtMH6KAu9RfPECBfaYzKBJvMYIB9zCCAfMCAQEwVjBRMQswCQYDVQQGEwJK
+# UDEOMAwGA1UECAwFT3Nha2ExEjAQBgNVBAoMCVByb2plY3QtSzEeMBwGA1UEAwwV
+# Y2EucHJvamVjdC1rLm15ZG5zLmpwAgEGMAkGBSsOAwIaBQCgeDAYBgorBgEEAYI3
+# AgEMMQowCKACgAChAoAAMBkGCSqGSIb3DQEJAzEMBgorBgEEAYI3AgEEMBwGCisG
+# AQQBgjcCAQsxDjAMBgorBgEEAYI3AgEWMCMGCSqGSIb3DQEJBDEWBBRRCSbl25Xe
+# gM0v5LbvmD725oGASDANBgkqhkiG9w0BAQEFAASCAQDZ38gTKKb7eU0HZ/l9r4tX
+# m5BPndx0LoTg+GiI7TUTXvKj0Gkdqs4+QUqmq4vvS7xD7doZ8rgdPPE7ogj+Qr7n
+# yvS208wasDig7AVFxl2Qkymo0QLF8g60FPegrnlmm+MSM3KRibKzJEAWUoyuWrB0
+# 6/c+vU3dEIpXO7shvRAxdeZxNalM6jlgbfZ6jz+EJP+l2QvCXB9LA3Q9d7cuj6is
+# Gy0lEVzMSMwYo7S0wy9+UodKY6x3FaYTllL0BCK/h6ppUwaXrf8zsvs4F3EhRaCi
+# mCRdEMo7HhIAcC3os+xjKCjGXBVhsPcB2l4373MnWDvWSUgLrDEJ8nL7o6Ju9jup
 # SIG # End signature block
