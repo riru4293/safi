a2bc11a228944b19d119f1aa7b1f04c261db1a85
riru <riru4293@gmail.com>
2025-01-13 00:15:43 +0900

    Update install.ps1

--- setup/MariaDB/install.ps1
+++ setup/MariaDB/install.ps1
@@ -1,25 +1,25 @@
-$ErrorActionPreference = "Stop"
+${ErrorActionPreference} = "Stop"
 
-$PREFIX = "$env:LOCALAPPDATA\Programs\MariaDB"
-$VER = '11.6.2'
-$ORIGIN_NAME = "mariadb-${VER}-winx64"
-$NAME = "${VER}"
-$DEST = "${PREFIX}\${NAME}"
-$SRC = "https://mirrors.xtom.jp/mariadb//mariadb-${VER}/winx64-packages/mariadb-${VER}-winx64.zip"
-$SRC_HASH = 'FDA36DC30BD10979BA1496D08C2AEE4549833A09BF26AA914B653AB792A9ACBE'
-$HASH_ALG = 'SHA256' # [MD5|SHA1|SHA256|SHA512]
-$ZIP = "$env:TMP\$(${SRC}.Substring(${SRC}.LastIndexOf("/") + 1))"
+${PREFIX} = "${env:LOCALAPPDATA}\Programs\MariaDB"
+${VER} = '11.6.2'
+${ORIGIN_NAME} = "mariadb-${VER}-winx64"
+${NAME} = "${VER}"
+${DEST} = "${PREFIX}\${NAME}"
+${SRC} = "https://mirrors.xtom.jp/mariadb//mariadb-${VER}/winx64-packages/mariadb-${VER}-winx64.zip"
+${SRC_HASH} = 'FDA36DC30BD10979BA1496D08C2AEE4549833A09BF26AA914B653AB792A9ACBE'
+${HASH_ALG} = 'SHA256' # [MD5|SHA1|SHA256|SHA512]
+${ZIP} = "${env:TMP}\$(${SRC}.Substring(${SRC}.LastIndexOf("/") + 1))"
 
 # https://mirror.mariadb.org/zoneinfo/
-$TZ_SRC = "https://mirrors.xtom.jp/mariadb/zoneinfo/zoneinfo.zip"
-$TZ_SRC_HASH = '2bac8646c92314f8d6c30a0e05bce8355091a7bbb0b6b7b058c9363378c9597a'
-$TZ_HASH_ALG = 'SHA256' # [MD5|SHA1|SHA256|SHA512]
-$TZ_ZIP = "$env:TMP\$(${TZ_SRC}.Substring(${TZ_SRC}.LastIndexOf("/") + 1))"
-$TZ_SQL = "${DEST}\zoneinfo.sql"
+${TZ_SRC} = "https://mirrors.xtom.jp/mariadb/zoneinfo/zoneinfo.zip"
+${TZ_SRC_HASH} = '2bac8646c92314f8d6c30a0e05bce8355091a7bbb0b6b7b058c9363378c9597a'
+${TZ_HASH_ALG} = 'SHA256' # [MD5|SHA1|SHA256|SHA512]
+${TZ_ZIP} = "${env:TMP}\$(${TZ_SRC}.Substring(${TZ_SRC}.LastIndexOf("/") + 1))"
+${TZ_SQL} = "${DEST}\zoneinfo.sql"
 
 
 # Download
-$RequireDownload = -Not( Test-Path "${ZIP}" ) `
+${RequireDownload} = -Not( Test-Path "${ZIP}" ) `
   -or ${SRC_HASH}.ToUpper() -ne ( Get-FileHash -Path "${ZIP}" -Algorithm ${HASH_ALG} ).Hash
 
 if( ${RequireDownload} ) {
@@ -31,7 +31,7 @@ if( ${RequireDownload} ) {
 
 
 # Download time zones
-$RequireDownload = -Not( Test-Path "${TZ_ZIP}" ) `
+${RequireDownload} = -Not( Test-Path "${TZ_ZIP}" ) `
   -or ${TZ_SRC_HASH}.ToUpper() -ne ( Get-FileHash -Path "${TZ_ZIP}" -Algorithm ${TZ_HASH_ALG} ).Hash
 
 if( ${RequireDownload} ) {
