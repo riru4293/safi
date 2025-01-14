ef2efa5573d70809bc3236e011594d4f949b2ea0
riru <riru4293@gmail.com>
2025-01-14 20:30:01 +0900

    Refactoring SelfSignedCert install scripts

--- setup/SelfSignedCertificate/install.bat
+++ setup/SelfSignedCertificate/install.bat
@@ -1,3 +1,26 @@
-pwsh -ExecutionPolicy RemoteSigned -File "%~dp0%~n0.ps1"
+@ECHO OFF
 
-TIMEOUT /T -1
+REM Get installed PowerShell versions
+FOR /F "USEBACKQ TOKENS=*" %%A in ( `pwsh -Command "$PSVersionTable.PSVersion.Major" 2^>NUL` ) do (
+    SET PS_VER=%%A
+)
+
+REM Confirm PowerShell 7 is installed
+IF DEFINED PS_VER IF %PS_VER% GEQ 7 (
+    SETLOCAL ENABLEDELAYEDEXPANSION
+
+    REM Escape arguments
+    SET "ARGS="
+    FOR %%A IN ( %* ) DO (
+        SET "ESCAPED_ARG=%%~A"
+        SET "ESCAPED_ARG=!ESCAPED_ARG:"=\"!"
+        SET "ARGS=!ARGS! "!ESCAPED_ARG!""
+    )
+
+    pwsh -ExecutionPolicy RemoteSigned -File "%~dp0%~n0.ps1" !ARGS!
+
+    ENDLOCAL
+) ELSE (
+    ECHO [Error] Missing PowerShell 7 or higher
+    TIMEOUT /T -1
+)
--- setup/SelfSignedCertificate/install.ps1
+++ setup/SelfSignedCertificate/install.ps1
@@ -1,29 +1,32 @@
 #Requires -Version 7
-$ErrorActionPreference = 'Stop'
+Param(
+  [switch]${WithCertCreation},
+  [switch]${WithoutOpensslInstallation}
+)
 
-# Basic variables
+${ErrorActionPreference} = 'Continue'
+
+# OpenSSL configuration
 ${PREFIX} = "${env:LOCALAPPDATA}\Programs\OpenSSL"
-${VER} = "3.4.0"
+${VER} = '3.4.0'
 ${DEST} = "${PREFIX}\${VER}"
-${CONF} = "${DEST}\ssl\openssl.cnf"
+
+${OPENSSL_HOME} = "${DEST}"
+${OPENSSL_ORIGIN_CONF} = "${OPENSSL_HOME}\ssl\openssl.cnf"
+
+# See: https://kb.firedaemon.com/support/solutions/articles/4000121705
+${SRC} = "https://download.firedaemon.com/FireDaemon-OpenSSL/openssl-${VER}.zip"
+${ZIP} = "${env:TMP}\$( Split-Path -Path ${SRC} -Leaf )"
+${SRC_HASH} = 'FFF7C56C1588644A609F63F60FAD15275E2CE6FFC310C320CDCB72554449CCA5'.ToUpper()
+${HASH_ALG} = 'SHA256' # [MD5|SHA1|SHA256|SHA512]
+
+# Certificate configuration
 ${COUNTRY} = 'JP'
 ${STATION} = 'Osaka'
 ${ORG} = 'project-k'
 ${DOMAIN} = 'project-k.mydns.jp'
 
-# Environment variables
-${env:PATH} = "${DEST}\$( [System.Environment]::Is64BitProcess ? 'x64' : 'x86' )\bin" + ';' + "${env:PATH}"
-${env:CA_HOME} = "${env:LOCALAPPDATA}\CA"
-
-# OpenSSL source
-# See: https://kb.firedaemon.com/support/solutions/articles/4000121705
-${SRC} = "https://download.firedaemon.com/FireDaemon-OpenSSL/openssl-${VER}.zip"
-${SRC_HASH} = 'FFF7C56C1588644A609F63F60FAD15275E2CE6FFC310C320CDCB72554449CCA5'.ToUpper()
-${HASH_ALG} = 'SHA256' # [MD5|SHA1|SHA256|SHA512]
-${ZIP} = "${env:TMP}\$( Split-Path -Path ${SRC} -Leaf )"
-
 # CA configuration
-${CA_PASS} = 'Please change'
 ${CA_CONF} = "${env:CA_HOME}\openssl.ca.cnf"
 ${CA_SUBJ} = "/C=${COUNTRY}/ST=${STATION}/O=${ORG}/CN=ca.${DOMAIN}"
 ${CA_CERT_NAME} = "ca.${ORG}"
@@ -32,82 +35,86 @@ ${CA_DAYS} = 3650
 # Server configuration
 ${SV_DAYS} = 1000
 ${SV_NAMES} = @(
-  @{ NAME = ${env:COMPUTERNAME}.ToLower() }
-  @{ NAME = 'cure'      }
-  @{ NAME = 'blizzard'  }
-  @{ NAME = 'poison'    }
-  @{ NAME = 'toad'      }
-  @{ NAME = 'meteor'    }
+  ${env:COMPUTERNAME}.ToLower()
+  'cure'
+  'poison'
+  'toad'
+  'meteor'
 )
+
+# Environment variables
+${env:PATH} = "${DEST}\x64\bin;${env:PATH}"
+if ( -Not ${env:CA_HOME} ) { ${env:CA_HOME} = "${env:LOCALAPPDATA}\CA" }
 # --------------------------------------------------
-
-
-# Abort if already exists a CA
-if( Test-Path "${env:CA_HOME}" )
-{
-  Write-Host "Abort because already exists a CA. [ ${env:CA_HOME} ]"
-  Read-Host 'Press enter to exit'
-  exit 9
-}
+Write-Host "CA_HOME: ${env:CA_HOME}"
 
 
 # To be running again as administrators if not administrators.
-${u} = [Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()
+${currentUser} = [Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()
 
-if ( -Not( ${u} ).IsInRole( 'Administrators' ) ) {
+if ( -Not( ${currentUser} ).IsInRole( [System.Security.Principal.WindowsBuiltInRole]::Administrator ) ) {
   Start-Process -Verb RunAs -FilePath pwsh -ArgumentList @(
-    '-ExecutionPolicy', 'RemoteSigned', '-File', "`"${PSCommandPath}`""
+    '-ExecutionPolicy', 'RemoteSigned'
+    '-File', "`"${PSCommandPath}`""
+    $( if( ${WithCertCreation} ) { '-WithCertCreation' } )
+    $( if( ${WithoutOpensslInstallation} ) { '-WithoutOpensslInstallation' } )
   )
   exit
 }
 
 
-# Download OpenSSL
-${d} = -Not( Test-Path "${ZIP}" ) -or -Not( "${SRC_HASH}" -eq ( Get-FileHash -Path "${ZIP}" -Algorithm "${HASH_ALG}" ).Hash )
+# Install OpenSSL if specified
+if ( -Not ${WithoutOpensslInstallation} ) {
 
-if( ${d} ) {
+  # Download OpenSSL
+  ${requireDownload} = -Not ( Test-Path "${ZIP}" ) -Or `
+    -Not ( "${SRC_HASH}" -eq ( Get-FileHash -Path "${ZIP}" -Algorithm "${HASH_ALG}" ).Hash )
+  
   Write-Host "Downloading to ${ZIP} from ${SRC}"
-  Invoke-WebRequest -Uri "${SRC}" -outfile "${ZIP}"
-} else {
-  Write-Host 'Skip download OpenSSL. Because already exists.'
+  if( ${requireDownload} ) {
+    Invoke-WebRequest -Uri "${SRC}" -outfile "${ZIP}"
+  } else {
+    Write-Host 'Skip download OpenSSL. Because already exists.'
+  }
+  
+  # Extract OpenSSL
+  Write-Host "Extracting to ${DEST} from ${ZIP}"
+  if( Test-Path "${DEST}" ) { Remove-Item "${DEST}" -Recurse -Force }
+  Expand-Archive -Path "${ZIP}" -DestinationPath "${DEST}"
+  
+  # Create junctions
+  Write-Host "Create junction to ${DEST} from ${PREFIX}\primary"
+  if( Test-Path "${PREFIX}\primary" ) { ( Get-Item "${PREFIX}\primary" ).Delete() }
+  New-Item -Path "${PREFIX}" -Name primary -Value "${DEST}" -ItemType Junction
 }
 
 
-# Extract OpenSSL
-Write-Host "Extracting to ${DEST} from ${ZIP}"
-if( Test-Path "${DEST}" ) { Remove-Item "${DEST}" -Recurse -Force }
-Expand-Archive -Path "${ZIP}" -DestinationPath "${DEST}"
+# Create certs if specified
+if ( ${WithCertCreation} ) {
+  # Get CA passphrase
+  ${CaPass} = Read-Host -Prompt 'Enter CA passphrase' -AsSecureString
+  
+  # Create CA directories
+  if( Test-Path "${env:CA_HOME}" ) { }
+  New-Item -Type Directory -Path "${env:CA_HOME}\private"
+  New-Item -Type Directory -Path "${env:CA_HOME}\crl"
+  New-Item -Type Directory -Path "${env:CA_HOME}\certs"
+  New-Item -Type Directory -Path "${env:CA_HOME}\newcerts"
+  New-Item -Type File -Path "${env:CA_HOME}\index.txt"
+  Set-Content -Value ( [Text.Encoding]::UTF8.GetBytes( '01' ) ) `
+    -AsByteStream -Path "${env:CA_HOME}\serial"
 
-
-# Create junctions
-Write-Host "Create junction to ${DEST} from ${PREFIX}\primary"
-if( Test-Path "${PREFIX}\primary" ) { ( Get-Item "${PREFIX}\primary" ).Delete() }
-New-Item -Path "${PREFIX}" -Name primary -Value "${DEST}" -ItemType Junction
-
-
-# Uninstall CA if exists
-${c} = Get-ChildItem -Path Cert:\LocalMachine\Root | Where-Object { $_.Subject -like "*CN=ca.${DOMAIN},*" }
-
-if ( ${c} ) {
-  ${c} | ForEach-Object{ Remove-Item -Path "Cert:\LocalMachine\Root\$( $_.Thumbprint )" }
-}
-
-
-# Create CA directories
-New-Item -Type Directory -Path "${env:CA_HOME}\private"
-New-Item -Type Directory -Path "${env:CA_HOME}\crl"
-New-Item -Type Directory -Path "${env:CA_HOME}\certs"
-New-Item -Type Directory -Path "${env:CA_HOME}\newcerts"
-New-Item -Type File -Path "${env:CA_HOME}\index.txt"
-Set-Content -Value ( [Text.Encoding]::UTF8.GetBytes( '01' ) ) -AsByteStream -Path "${env:CA_HOME}\serial"
-Set-Content -Value ( [Text.Encoding]::UTF8.GetBytes( "${CA_PASS}" ) ) -AsByteStream -Path "${env:CA_HOME}\.capass"
-
-
-# OpenSSL configuration for CA
-$( Get-Content "${CONF}" ) | `
-  ForEach-Object { $_ -replace './demoCA', '$ENV::CA_HOME' } | `
-  Set-Content "${CA_CONF}"
-
+  # Set CA passphrase
+  ${decoded} = [System.Runtime.InteropServices.Marshal]::SecureStringToBSTR( ${CaPass} )
+  Set-Content -Value ( [Text.Encoding]::UTF8.GetBytes( `
+    "$( [System.Runtime.InteropServices.Marshal]::PtrToStringBSTR( ${decoded} ) )" ) ) `
+    -AsByteStream -Path "${env:CA_HOME}\.capass"
+  
+  # OpenSSL configuration for CA
+  $( Get-Content "${OPENSSL_ORIGIN_CONF}" ) | `
+    ForEach-Object { $_ -replace './demoCA', '$ENV::CA_HOME' } | `
+    Set-Content "${CA_CONF}"
+  
 @"
 
 [ v3_self_ca ]
@@ -115,30 +122,28 @@ subjectKeyIdentifier=hash
 authorityKeyIdentifier=keyid:always,issuer
 basicConstraints = CA:true
 keyUsage = cRLSign, keyCertSign
-"@ | ForEach-Object{ [Text.Encoding]::UTF8.GetBytes( $_ + "`n" ) } | Add-Content -AsByteStream -Path "${CA_CONF}"
+"@ `
+    | ForEach-Object{ [Text.Encoding]::UTF8.GetBytes( $_ + "`n" ) } `
+    | Add-Content -AsByteStream -Path "${CA_CONF}"
+  
+  # Create CA cert
+  Write-Host 'Create CA cert'
 
-
-# Create CA cert
-openssl req -new -x509 -extensions v3_self_ca -config "${CA_CONF}" -subj "${CA_SUBJ}" `
--keyout "${env:CA_HOME}\private\${CA_CERT_NAME}.key" -out "${env:CA_HOME}\certs\${CA_CERT_NAME}.crt" `
--passout file:"${env:CA_HOME}\.capass" -days ${CA_DAYS}
-
-openssl x509 -in "${env:CA_HOME}\certs\${CA_CERT_NAME}.crt" -text
-
-
-# Install CA
-Import-Certificate -FilePath "${env:CA_HOME}\certs\${CA_CERT_NAME}.crt" -CertStoreLocation Cert:\LocalMachine\Root
-
-
-# Create servers certs
-foreach ( ${s} in ${SV_NAMES} ) {
-  ${n} = ${s}.NAME
-
-  # OpenSSL configuration for server
-  Get-Content "${CONF}" | ForEach-Object { $_ -replace './demoCA', '$ENV::CA_HOME' } `
-  | Set-Content -Path "${env:CA_HOME}\openssl.${n}.cnf"
-
-  @"
+  openssl req -new -x509 -extensions v3_self_ca -config "${CA_CONF}" -subj "${CA_SUBJ}" `
+  -keyout "${env:CA_HOME}\private\${CA_CERT_NAME}.key" -out "${env:CA_HOME}\certs\${CA_CERT_NAME}.crt" `
+  -passout file:"${env:CA_HOME}\.capass" -days ${CA_DAYS}
+  
+  openssl x509 -in "${env:CA_HOME}\certs\${CA_CERT_NAME}.crt" -text
+  
+  
+  # Create servers certs
+  foreach ( ${serverName} in ${SV_NAMES} ) {
+  
+    # OpenSSL configuration for server
+    Get-Content "${OPENSSL_ORIGIN_CONF}" | ForEach-Object { $_ -replace './demoCA', '$ENV::CA_HOME' } `
+    | Set-Content -Path "${env:CA_HOME}\openssl.${serverName}.cnf"
+  
+@"
 
 [ v3_self_server ]
 basicConstraints=CA:FALSE
@@ -146,30 +151,44 @@ keyUsage = digitalSignature, keyEncipherment
 extendedKeyUsage = serverAuth
 subjectKeyIdentifier=hash
 authorityKeyIdentifier=keyid,issuer
-subjectAltName=DNS:${n}.${DOMAIN},DNS:${n},DNS:localhost,IP:127.0.0.1
+subjectAltName=DNS:${serverName}.${DOMAIN},DNS:${serverName},DNS:localhost,IP:127.0.0.1
 "@ `
-  | ForEach-Object{ [Text.Encoding]::UTF8.GetBytes( $_ + "`n" ) } `
-  | Add-Content -AsByteStream -Path "${env:CA_HOME}\openssl.${n}.cnf"
+      | ForEach-Object{ [Text.Encoding]::UTF8.GetBytes( $_ + "`n" ) } `
+      | Add-Content -AsByteStream -Path "${env:CA_HOME}\openssl.${serverName}.cnf"
+  
+    ## Create server cert
+    Write-Host "Create server cert: ${serverName}"
 
-  ## Create server cert
-  openssl genrsa -out "${env:CA_HOME}\private\${n}.${ORG}.key" 2048
-
-  openssl req -new -config "${env:CA_HOME}\openssl.${n}.cnf" -key "${env:CA_HOME}\private\${n}.${ORG}.key" `
-  -out "${env:CA_HOME}\certs\${n}.${ORG}.csr" -subj "/C=${COUNTRY}/ST=${STATION}/O=${ORG}/CN=${n}.${DOMAIN}"
-
-  openssl ca -batch -extensions v3_self_server -config "${env:CA_HOME}\openssl.${n}.cnf" `
-  -cert "${env:CA_HOME}\certs\${CA_CERT_NAME}.crt" -keyfile "${env:CA_HOME}\private\${CA_CERT_NAME}.key" `
-  -passin file:"${env:CA_HOME}\.capass" -out "${env:CA_HOME}\certs\${n}.${ORG}.crt" -days ${SV_DAYS} `
-  -infiles "${env:CA_HOME}\certs\${n}.${ORG}.csr"
+    openssl genrsa -out "${env:CA_HOME}\private\${serverName}.${ORG}.key" 2048
+  
+    openssl req -new -config "${env:CA_HOME}\openssl.${serverName}.cnf" -key "${env:CA_HOME}\private\${serverName}.${ORG}.key" `
+    -out "${env:CA_HOME}\certs\${serverName}.${ORG}.csr" -subj "/C=${COUNTRY}/ST=${STATION}/O=${ORG}/CN=${serverName}.${DOMAIN}"
+  
+    openssl ca -batch -extensions v3_self_server -config "${env:CA_HOME}\openssl.${serverName}.cnf" `
+    -cert "${env:CA_HOME}\certs\${CA_CERT_NAME}.crt" -keyfile "${env:CA_HOME}\private\${CA_CERT_NAME}.key" `
+    -passin file:"${env:CA_HOME}\.capass" -out "${env:CA_HOME}\certs\${serverName}.${ORG}.crt" -days ${SV_DAYS} `
+    -infiles "${env:CA_HOME}\certs\${serverName}.${ORG}.csr"
+  }
 }
 
-Read-Host 'Press enter to exit'
+Write-Host 'Install CA cert'
+
+# Uninstall CA if exists
+Get-ChildItem -Path Cert:\LocalMachine\Root | Where-Object { $_.Subject -like "*CN=ca.${DOMAIN},*" } `
+  | ForEach-Object{ Remove-Item -Path "Cert:\LocalMachine\Root\$( $_.Thumbprint )" }
+
+  
+# Install CA
+Import-Certificate -FilePath "${env:CA_HOME}\certs\${CA_CERT_NAME}.crt" `
+  -CertStoreLocation Cert:\LocalMachine\Root
+
+Read-Host "Press enter to exit"
 
 # SIG # Begin signature block
 # MIIGXAYJKoZIhvcNAQcCoIIGTTCCBkkCAQExCzAJBgUrDgMCGgUAMGkGCisGAQQB
 # gjcCAQSgWzBZMDQGCisGAQQBgjcCAR4wJgIDAQAABBAfzDtgWUsITrck0sYpfvNR
-# AgEAAgEAAgEAAgEAAgEAMCEwCQYFKw4DAhoFAAQU5jOoT3CdqYKcccC8FWNp+25K
-# T0KgggPPMIIDyzCCArOgAwIBAgIBBjANBgkqhkiG9w0BAQsFADBRMQswCQYDVQQG
+# AgEAAgEAAgEAAgEAAgEAMCEwCQYFKw4DAhoFAAQU87U11zDH/9BgV5KDofy9L9DL
+# ylmgggPPMIIDyzCCArOgAwIBAgIBBjANBgkqhkiG9w0BAQsFADBRMQswCQYDVQQG
 # EwJKUDEOMAwGA1UECAwFT3Nha2ExEjAQBgNVBAoMCVByb2plY3QtSzEeMBwGA1UE
 # AwwVY2EucHJvamVjdC1rLm15ZG5zLmpwMB4XDTI1MDEwMjEzNDcxN1oXDTI3MDky
 # OTEzNDcxN1owYzELMAkGA1UEBhMCSlAxDjAMBgNVBAgMBU9zYWthMRIwEAYDVQQK
@@ -193,11 +212,11 @@ Read-Host 'Press enter to exit'
 # UDEOMAwGA1UECAwFT3Nha2ExEjAQBgNVBAoMCVByb2plY3QtSzEeMBwGA1UEAwwV
 # Y2EucHJvamVjdC1rLm15ZG5zLmpwAgEGMAkGBSsOAwIaBQCgeDAYBgorBgEEAYI3
 # AgEMMQowCKACgAChAoAAMBkGCSqGSIb3DQEJAzEMBgorBgEEAYI3AgEEMBwGCisG
-# AQQBgjcCAQsxDjAMBgorBgEEAYI3AgEWMCMGCSqGSIb3DQEJBDEWBBRcIJNty6t8
-# 7egLlChTarHkzxgxGTANBgkqhkiG9w0BAQEFAASCAQAqqabPaWW56wyswyH6LnKG
-# MU+PfGaAPv1Ao3JvDBgpFGM5/n4iDKJ+oSPRUMzlFpRHxlta0Ec02sDzyfZND8SI
-# JLpziIfBcAmKWmJpNTwYwzCvq+KPWVfjNr14I96Tkk+p0xpq02oiv0GUAuYsatFG
-# 1XQpMAbGDljCfhkTfYWeZbJyBa6NrAVOv19zX3gYoDf5vrWVFbLw5zf9kXk9Y7p6
-# I1N0VZSepNz+/PBv50/c/OuT3SK3Q9byfs5uadOEVqDm8B0ZLoyuQ7xOWqFCaI1e
-# RKPn24ZYTOp2u/ptDKeO5djg5N768ahUbntFr/PyrujRUl7bbS+jaLV87w55v552
+# AQQBgjcCAQsxDjAMBgorBgEEAYI3AgEWMCMGCSqGSIb3DQEJBDEWBBTsFZizfIik
+# wIBTqByUZz/ql44B2TANBgkqhkiG9w0BAQEFAASCAQBLcEPgZbaC5FY4kW3xdY2Y
+# Sfl8msRLDe0ONqcSvO3XjkJF4W5YGeMATGk6DS7xGbQvMBxv46r7ueN6ESNfaf6f
+# rUs+3RL8+dWWHUpoz7wXaLSMBrG8YVEDrITO8xBbgKmzCP68B/GNRvkb6MwgxlEA
+# H/Ca6VyjuaQ/9VSPA38cUz6WOZUherYsk8eKQwtifUto0Q+GPD0EDRYQHE+FqDsp
+# ugDmb8hPTQ4ES6xaxo9haNtziuIdhBRoJWkr8CIJUngPXHpOlB+DBwJfOnkI9LKf
+# Zs8FaREkKa4mIgFR61Fas2v9QDefCelpSZZzRAePGXAiyzd0g5j6EIQbdmkFzNj6
 # SIG # End signature block
