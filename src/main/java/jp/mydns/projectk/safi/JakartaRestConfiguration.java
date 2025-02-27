cb8069c7897c5fa2a15b981664459a0aac3d1c11
riru <riru4293@gmail.com>
2025-02-27 20:22:00 +0900

    Add library for swagger
    Delete module-info
    Because conflict unknown reasons.

--- src/main/java/jp/mydns/projectk/safi/JakartaRestConfiguration.java
+++ src/main/java/jp/mydns/projectk/safi/JakartaRestConfiguration.java
@@ -25,13 +25,13 @@
  */
 package jp.mydns.projectk.safi;
 
-//import io.swagger.v3.oas.annotations.OpenAPIDefinition;
-//import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
-//import io.swagger.v3.oas.annotations.info.Contact;
-//import io.swagger.v3.oas.annotations.info.Info;
-//import io.swagger.v3.oas.annotations.info.License;
-//import io.swagger.v3.oas.annotations.security.SecurityScheme;
-//import io.swagger.v3.oas.annotations.servers.Server;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.annotations.servers.Server;
 import jakarta.ws.rs.ApplicationPath;
 import jakarta.ws.rs.core.Application;
 
