90a4a09929b39186b7d3a41f67ef3b5650b549a5
riru <riru4293@gmail.com>
2025-03-16 20:06:29 +0900

    temp

--- src/main/java/jp/mydns/projectk/safi/value/trial/SchedefValue.java
+++ src/main/java/jp/mydns/projectk/safi/value/trial/SchedefValue.java
@@ -25,10 +25,131 @@
  */
 package jp.mydns.projectk.safi.value.trial;
 
-/**
- *
- * @author riru
- */
-public interface SchedefValue {
+import jakarta.json.JsonObject;
+import jakarta.json.JsonString;
+import jakarta.json.JsonValue;
+import jp.mydns.projectk.safi.constant.SchedefKing;
+import jp.mydns.projectk.safi.entity.SchedefEntity;
+import jp.mydns.projectk.safi.value.ValidityPeriodValue;
 
+/**
+ * Schedule definition.
+ *
+ * @implSpec This class is immutable and thread-safe.
+ * @author riru
+ * @version 3.0.0
+ * @since 3.0.0
+ */
+public class SchedefValue {
+
+    private final String id;
+    private final String jobdefId;
+    private final ValidityPeriodValue validityPeriod;
+    private final String priority;
+    private final SchedefKing type;
+    private final String name;
+    private final JsonObject value;
+
+    /**
+     * Constructor.
+     *
+     * @param entity the {@code MSchedef}
+     * @throws NullPointerException if {@code entity} is {@code null}
+     * @throws IllegalArgumentException if schedule definition type does not exist
+     * @since 3.0.0
+     */
+    public SchedefValue(SchedefEntity entity) {
+        JsonValue rawType = entity.getValue().unwrap().asJsonObject().get("type");
+        if (rawType == null || rawType.getValueType() != JsonValue.ValueType.STRING) {
+            throw new IllegalArgumentException("Schedule definition type does not exist.");
+        }
+        this.id = entity.getId();
+        this.priority = entity.getPriority();
+        this.value = entity.getValue();
+        this.type = JsonString.class.cast(rawType).getString();
+        this.jobdefId = entity.getJobdefId();
+    }
+
+    /**
+     * Get schedule definition id.
+     *
+     * @return schedule definition id
+     * @since 3.0.0
+     */
+    public String getId() {
+        return id;
+    }
+
+    /**
+     * Get job definition id.
+     *
+     * @return job definition id
+     * @since 3.0.0
+     */
+    public String getJobdefId() {
+        return jobdefId;
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * @since 3.0.0
+     */
+    @Override
+    public ValidityPeriodValue getValidityPeriod() {
+        return validityPeriod;
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * @since 3.0.0
+     */
+    @Override
+    public Optional<String> getName() {
+        return Optional.ofNullable(name);
+    }
+
+    /**
+     * Get schedule definition priority.
+     *
+     * @return schedule definition priority
+     * @since 3.0.0
+     */
+    public String getPriority() {
+        return priority;
+    }
+
+    /**
+     * Get schedule definition type.
+     *
+     * @return schedule definition type
+     * @since 3.0.0
+     */
+    public String getType() {
+        return type;
+    }
+
+    /**
+     * Get schedule definition value.
+     *
+     * @return schedule definition value
+     * @since 3.0.0
+     */
+    public JsonObject getValue() {
+        return value;
+    }
+
+    /**
+     * Returns a string representation of this.
+     *
+     * @return a string representation
+     * @since 3.0.0
+     */
+    @Override
+    public String toString() {
+        return "SchedefValue{" + "id=" + id + ", priority=" + priority
+            + ", type=" + type + ", assignables=" + assignables
+            + ", jobdefId=" + jobdefId + ", value=" + value + '}';
+    }
 }
