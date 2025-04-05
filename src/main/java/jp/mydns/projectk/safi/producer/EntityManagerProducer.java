80805de93506ad6caba660d9c0dcfab8908d713d
riru <riru4293@gmail.com>
2025-04-05 10:56:25 +0900

    Fix issues that entity manager dispose
    Remove dispose implements.
    Because em is managed by JPA. It is not  managed CDI.

--- src/main/java/jp/mydns/projectk/safi/producer/EntityManagerProducer.java
+++ src/main/java/jp/mydns/projectk/safi/producer/EntityManagerProducer.java
@@ -26,7 +26,6 @@
 package jp.mydns.projectk.safi.producer;
 
 import jakarta.enterprise.context.RequestScoped;
-import jakarta.enterprise.inject.Disposes;
 import jakarta.enterprise.inject.Produces;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
@@ -54,15 +53,5 @@ public class EntityManagerProducer {
     @RequestScoped
     public EntityManager produce() {
         return em;
-    }
-
-    /**
-     * Close the produced {@code EntityManager} if disposed.
-     *
-     * @param em the produced {@code EntityManager}
-     * @since 3.0.0
-     */
-    public void close(@Disposes EntityManager em) {
-        em.close();
     }
 }
