8619fe12b5f30319463f2d033d953ad859da2f87
riru <riru4293@gmail.com>
2025-04-17 21:26:00 +0900

    Add error response context

--- src/main/java/jp/mydns/projectk/safi/resource/trial/ErrorResponseContext.java
+++ src/main/java/jp/mydns/projectk/safi/resource/trial/ErrorResponseContext.java
@@ -25,27 +25,197 @@
  */
 package jp.mydns.projectk.safi.resource.trial;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+import static io.swagger.v3.oas.annotations.media.Schema.AccessMode.READ_ONLY;
+import static io.swagger.v3.oas.annotations.media.Schema.RequiredMode.NOT_REQUIRED;
 import jakarta.json.JsonObject;
 import jakarta.json.bind.annotation.JsonbProperty;
+import jakarta.validation.constraints.NotNull;
 import java.net.URI;
 import java.util.List;
+import java.util.Optional;
+import jp.mydns.projectk.safi.util.CollectionUtils;
+import jp.mydns.projectk.safi.value.ValueTemplate;
 
 /**
+ * Error response information.
+ *
+ * <p>
+ * Implementation requirements.
+ * <ul>
+ * <li>This class is immutable and thread-safe.</li>
+ * <li>Can be converted to JSON.</li>
+ * </ul>
+ *
+ * <a href="{@docRoot}/../schemas/error-response-context.schema.json">Json schema is here</a>
  *
  * @author riru
+ * @version 3.0.0
+ * @since 3.0.0
  */
-public interface ErrorResponseContext {
+public interface ErrorResponseContext extends ValueTemplate {
 
+    /**
+     * Get JSON schema URI.
+     *
+     * @return the URI of this JSON schema
+     * @since 3.0.0
+     */
     @JsonbProperty("$schema")
-    // ReadOnly
-    URI getSchema();
+    @Schema(description = "", requiredMode = NOT_REQUIRED, accessMode = READ_ONLY)
+    @NotNull
+    Optional<URI> getSchema();
 
-    // ReadOnly
+    /**
+     * Get error code.
+     *
+     * @return error code
+     * @since 3.0.0
+     */
+    @Schema(description = "Error code.", accessMode = READ_ONLY)
+    @NotNull
     URI getCode();
 
-    // ReadOnly
+    /**
+     * Get error message.
+     *
+     * @return error message
+     * @since 3.0.0
+     */
+    @Schema(description = "Error message.", accessMode = READ_ONLY)
+    @NotNull
     String getMessage();
 
-    // ReadOnly
-    List<JsonObject> getDetails();
+    /**
+     * Get error details.
+     *
+     * @return error details
+     * @since 3.0.0
+     */
+    @Schema(description = "Error details.", requiredMode = NOT_REQUIRED, accessMode = READ_ONLY)
+    @NotNull
+    Optional<List<@NotNull JsonObject>> getDetails();
+
+    /**
+     * Builder of the {@code ErrorResponseContext}.
+     *
+     * @author riru
+     * @version 3.0.0
+     * @since 3.0.0
+     */
+    class Builder extends ValueTemplate.AbstractBuilder<Builder, ErrorResponseContext> {
+
+        private URI schema;
+        private URI code;
+        private String message;
+        private List<JsonObject> details;
+
+        /**
+         * Constructor.
+         *
+         * @since 3.0.0
+         */
+        public Builder() {
+            super(Builder.class);
+        }
+
+        /**
+         * Set JSON schema
+         *
+         * @param schema the JSON schema URI
+         * @return updated this
+         * @since 3.0.0
+         */
+        public Builder withSchema(URI schema) {
+            this.schema = schema;
+            return this;
+        }
+
+        /**
+         * Set error code.
+         *
+         * @param code error code
+         * @return updated this
+         * @since 3.0.0
+         */
+        public Builder withCode(URI code) {
+            this.code = code;
+            return this;
+        }
+
+        /**
+         * Set error message.
+         *
+         * @param message error message
+         * @return updated this
+         * @since 3.0.0
+         */
+        public Builder withMessage(String message) {
+            this.message = message;
+            return this;
+        }
+
+        /**
+         * Set error details.
+         *
+         * @param details error details
+         * @return updated this
+         * @since 3.0.0
+         */
+        public Builder withDetails(List<JsonObject> details) {
+            this.details = CollectionUtils.toUnmodifiable(details);
+            return this;
+        }
+
+        /**
+         * {@inheritDoc}
+         *
+         * @since 3.0.0
+         */
+        @Override
+        public ErrorResponseContext unsafeBuild() {
+            return new Bean(this);
+        }
+
+        private class Bean implements ErrorResponseContext {
+
+            private final URI schema;
+            private final URI code;
+            private final String message;
+            private final List<JsonObject> details;
+
+            public Bean(Builder builder) {
+                this.schema = builder.schema;
+                this.code = builder.code;
+                this.message = builder.message;
+                this.details = builder.details;
+            }
+
+            @Override
+            public Optional<URI> getSchema() {
+                return Optional.ofNullable(schema);
+            }
+
+            @Override
+            public URI getCode() {
+                return code;
+            }
+
+            @Override
+            public String getMessage() {
+                return message;
+            }
+
+            @Override
+            public Optional<List<JsonObject>> getDetails() {
+                return Optional.ofNullable(details);
+            }
+
+            @Override
+            public String toString() {
+                return "ErrorResponseContext{" + "schema=" + schema + ", code=" + code + ", message=" + message
+                    + ", details=" + details + '}';
+            }
+        }
+    }
 }
