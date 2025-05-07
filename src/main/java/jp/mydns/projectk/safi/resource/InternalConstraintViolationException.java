05086abda836811386bf99d33625b678bdc9151f
riru <riru4293@gmail.com>
2025-05-07 21:54:49 +0900

    Update InternalConstraintViolationException.java

--- src/main/java/jp/mydns/projectk/safi/resource/InternalConstraintViolationException.java
+++ src/main/java/jp/mydns/projectk/safi/resource/InternalConstraintViolationException.java
@@ -31,40 +31,42 @@ import jakarta.ws.rs.core.Response;
 import java.util.Objects;
 
 /**
- * A wrapper for {@code ConstraintViolationException} that occurs outside of direct user request data. Used to return
- * HTTP Status code 500 instead of 400. {@code ConstraintViolationException} that occurs outside of direct requests is
- * due to internal inconsistencies and is not the user's responsibility.
- *
- * @author riru
- * @version 3.0.0
- * @since 3.0.0
+ A wrapper for {@code ConstraintViolationException} that occurs outside of direct user request data.
+ Used to return HTTP Status code 500 instead of 400. {@code ConstraintViolationException} that
+ occurs outside of direct requests is due to internal inconsistencies and is not the user's
+ responsibility.
+
+ @author riru
+ @version 3.0.0
+ @since 3.0.0
  */
 public class InternalConstraintViolationException extends ServerErrorException {
 
-    @java.io.Serial
-    private static final long serialVersionUID = 8274928374928374923L;
+@java.io.Serial
+private static final long serialVersionUID = 8274928374928374923L;
 
-    private final ConstraintViolationException cvex;
+private final ConstraintViolationException cvex;
 
-    /**
-     * Construct with the {@code ConstraintViolationException}.
-     *
-     * @param cvex the {@code ConstraintViolationException}
-     * @throws NullPointerException if {@code cvex} is {@code null}
-     * @since 3.0.0
-     */
-    public InternalConstraintViolationException(ConstraintViolationException cvex) {
-        super(Response.Status.INTERNAL_SERVER_ERROR);
-        this.cvex = Objects.requireNonNull(cvex);
-    }
+/**
+ Construct with the {@code ConstraintViolationException}.
 
-    /**
-     * Get the {@code ConstraintViolationException}.
-     *
-     * @return the {@code ConstraintViolationException}
-     * @since 3.0.0
-     */
-    public ConstraintViolationException getConstraintViolationException() {
-        return cvex;
-    }
+ @param cvex the {@code ConstraintViolationException}
+ @throws NullPointerException if {@code cvex} is {@code null}
+ @since 3.0.0
+ */
+public InternalConstraintViolationException(ConstraintViolationException cvex) {
+    super(Response.Status.INTERNAL_SERVER_ERROR);
+    this.cvex = Objects.requireNonNull(cvex);
+}
+
+/**
+ Get the {@code ConstraintViolationException}.
+
+ @return the {@code ConstraintViolationException}
+ @since 3.0.0
+ */
+public ConstraintViolationException getConstraintViolationException() {
+    return cvex;
+}
+
 }
