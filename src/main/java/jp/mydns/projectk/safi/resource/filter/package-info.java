8e18c284aed70a636ade5b91d7e9e5d6b400d477
riru <riru4293@gmail.com>
2025-06-02 00:58:42 +0900

    Refactoring resource filters

--- src/main/java/jp/mydns/projectk/safi/resource/filter/package-info.java
+++ src/main/java/jp/mydns/projectk/safi/resource/filter/package-info.java
@@ -24,10 +24,10 @@
  * POSSIBILITY OF SUCH DAMAGE.
  */
 /**
- * Jakarta JAX-RS Resource Filter.
- * 
- * @author riru
- * @version 3.0.0
- * @since 3.0.0
+ Jakarta JAX-RS Resource Filter.
+
+ @author riru
+ @version 3.0.0
+ @since 3.0.0
  */
 package jp.mydns.projectk.safi.resource.filter;
