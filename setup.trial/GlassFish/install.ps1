5fa6a9f4d9f2ead935e423c41aab2aa066a73092
riru <riru4293@gmail.com>
2026-02-27 02:10:06 +0900

    Create install.ps1

---
+++ setup.trial/GlassFish/install.ps1
@@ -0,0 +1,229 @@
+#Requires -Version 7
+${ErrorActionPreference} = 'Stop'
+
+${LOCAL_ROOT} = "${env:USERPROFILE}\local"
+${JAVA_HOME} = "${LOCAL_ROOT}\opt\Java\jdk25"
+
+${MAJOR_VER} = 8
+${MINOR_VER} = 0
+${PATCH_VER} = 0
+
+${VER} = "${MAJOR_VER}.${MINOR_VER}.${PATCH_VER}"
+${SRC} = "https://download.eclipse.org/ee4j/glassfish/glassfish-${VER}.zip"
+${ZIP} = "${env:TMP}\$( Split-Path -Path ${SRC} -Leaf )"
+${ORIGIN_NAME} = "glassfish${MAJOR_VER}"
+
+${PREFIX} = "${LOCAL_ROOT}\opt\GlassFish"
+${NAME} = "${VER}"
+${DEST} = "${PREFIX}\${NAME}"
+
+${SVC_NAME} = 'SAFI_GLASSFISH'
+${GLASSFISH_HOME} = "${DEST}"
+
+${MARIACONN_VER} = '3.5.7'
+${MARIACONN_SRC} = "https://repo1.maven.org/maven2/org/mariadb/jdbc/mariadb-java-client/${MARIACONN_VER}/mariadb-java-client-${MARIACONN_VER}.jar"
+
+${SAFI_PASS} = 'Golden Hammer'
+${STORE_PASS} = 'changeit'
+
+# https://github.com/winsw/winsw/releases
+${WSW_VER} = '2.12.0'
+${WSW_ASSETS_SRC} = "https://api.github.com/repos/winsw/winsw/releases/tags/v${WSW_VER}"
+${WSW_ASSET_NAME} = 'WinSW.NET461.exe'
+${WSW_SRC} = 'Get it later with GitHub API'
+${WSW_EXE} = "${env:TMP}\WinSW_NET461_${WSW_VER}.exe"
+
+# --------------------------------------------------
+
+
+# To be running again as administrators if not administrators.
+${u} = [Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()
+
+if ( -Not( ${u} ).IsInRole( 'Administrators' ) ) {
+  Start-Process -Verb RunAs -FilePath pwsh -ArgumentList @(
+    '-ExecutionPolicy', 'RemoteSigned', '-File', "`"${PSCommandPath}`""
+  )
+  exit
+}
+
+
+# Remove an existing service
+if ( ${s} = Get-Service -Name "${SVC_NAME}" -ErrorAction SilentlyContinue ) {
+  if ( ${s}.Status -eq 'Running' ) {
+    Stop-Service -Name "${SVC_NAME}"
+  }
+
+  Remove-Service -Name "${SVC_NAME}"
+  Write-Host 'Removed existng service'
+}
+
+
+# Download GlassFish
+Write-Host "Downloading to ${ZIP} from ${SRC}"
+Invoke-WebRequest -Uri "${SRC}" -outfile "${ZIP}"
+
+
+# Download WinSW
+${r} = Invoke-RestMethod -Uri ${WSW_ASSETS_SRC} -Headers @{ 'User-Agent' = 'PowerShell' }
+${a} = ${r}.assets | Where-Object { $_.name -eq ${WSW_ASSET_NAME} }
+${WSW_SRC} = ${a}.browser_download_url
+
+Write-Host "Downloading to ${WSW_EXE} from ${WSW_SRC}"
+Invoke-WebRequest -Uri "${WSW_SRC}" -outfile "${WSW_EXE}" -Headers @{ 'User-Agent' = 'PowerShell' }
+
+
+# Extract GlassFish
+Write-Output "Extracting to ${PREFIX} from ${ZIP}"
+if( Test-Path "${PREFIX}\${ORIGIN_NAME}" ) { Remove-Item "${PREFIX}\${ORIGIN_NAME}" -Recurse -Force }
+if( Test-Path "${DEST}" ) { Remove-Item "${DEST}" -Recurse -Force }
+Expand-Archive -Path "${ZIP}" -DestinationPath "${PREFIX}"
+Rename-Item -Path "${PREFIX}\${ORIGIN_NAME}" -NewName "${NAME}"
+
+
+# Copy WinSW
+New-Item -ItemType Directory -Path "${DEST}\glassfish\domains\domain1\service"
+Write-Output "Copy to ${DEST}\glassfish\domains\domain1\service\glassfish_service.exe from ${WSW_EXE}"
+Copy-Item -Path "${WSW_EXE}" -Destination "${DEST}\glassfish\domains\domain1\service\glassfish_service.exe"
+
+
+# Download MariaDB Connector/J
+Invoke-WebRequest -Uri "${MARIACONN_SRC}" -outfile "${DEST}/glassfish/domains/domain1/lib/"
+
+
+# Configure AS_JAVA(JAVA_HOME)
+Add-Content -AsByteStream -Value ( [Text.Encoding]::UTF8.GetBytes( "SET `"AS_JAVA=${JAVA_HOME}`"" ) ) -Path "${DEST}/glassfish/config/asenv.bat"
+
+
+# Create junctions
+if( Test-Path "${PREFIX}\primary" ) { ( Get-Item "${PREFIX}\primary" ).Delete() }
+New-Item -Path "${PREFIX}" -Name primary -Value "${DEST}" -ItemType Junction
+
+
+# Install Service
+@"
+<service>
+  <id>${SVC_NAME}</id>
+  <name>${SVC_NAME}</name>
+  <startmode>Manual</startmode>
+  <description>GlassFish Server for SAFI</description>
+  <executable>$( ${GLASSFISH_HOME}.Replace( '\', '/' ) )/glassfish/lib/nadmin.bat</executable>
+  <logpath>$( ${GLASSFISH_HOME}.Replace( '\', '\\' ) )\\glassfish\\domains\\domain1\\service</logpath>
+  <logmode>reset</logmode>
+  <depend>tcpip</depend>
+  <startargument>start-domain</startargument>
+  <startargument>--watchdog</startargument>
+  <startargument>--domaindir</startargument>
+  <startargument>$( ${GLASSFISH_HOME}.Replace( '\', '\\' ) )\\glassfish\\domains</startargument>
+  <startargument>domain1</startargument>
+  <stopargument>stop-domain</stopargument>
+  <stopargument>--domaindir</stopargument>
+  <stopargument>$( ${GLASSFISH_HOME}.Replace( '\', '\\' ) )\\glassfish\\domains</stopargument>
+  <stopargument>domain1</stopargument>
+</service>
+"@ | Out-File -enc default "${DEST}\glassfish\domains\domain1\service\glassfish_service.xml"
+
+& "${DEST}\glassfish\domains\domain1\service\glassfish_service.exe" install
+
+Get-Service -Name "${SVC_NAME}" `
+| ForEach-Object { Start-Service -InputObject $_ } `
+| ForEach-Object { $_.WaitForStatus( 'Running', '00:02:00' ) }
+
+# Wait GlassFish domain1 running
+do {
+  Write-Host -NoNewLine '.'
+  Start-Sleep -Seconds 1
+  ${o} = ( & "${GLASSFISH_HOME}\bin\asadmin.bat" list-domains 2>&1 )
+} while ( -Not ( ${o} | Out-String | Select-String -Pattern 'domain1 running' ) )
+Write-Host 'Complete start domain1'
+
+# Configure JDBC
+& "${GLASSFISH_HOME}\bin\asadmin.bat" create-jdbc-connection-pool `
+--datasourceclassname=org.mariadb.jdbc.MariaDbDataSource `
+--restype=javax.sql.DataSource `
+--property=$(Write-Output $(Join-String -Separator ':' -InputObject @(`
+  'user=safi', `
+  "password=${SAFI_PASS}", `
+  'sessionVariables=innodb_lock_wait_timeout\=60', `
+  'sslMode=verify-full', `
+  "trustStore=file\:///$( ${LOCAL_ROOT}.Replace( '\', '/' ).Replace( ':', '\:' ) )/etc/tls/mariadb-connector-cacerts", `
+  'trustStoreType=pkcs12', `
+  "trustStorePassword=${STORE_PASS}", `
+  'URL=jdbc\:mariadb\://localhost/safi?serverTimezone\=UTC' `
+))) `
+SafiPool
+
+& "${GLASSFISH_HOME}\bin\asadmin.bat" set "resources.jdbc-connection-pool.SafiPool.statement-timeout-in-seconds=300"
+& "${GLASSFISH_HOME}\bin\asadmin.bat" set "resources.jdbc-connection-pool.SafiPool.statement-cache-size=20"
+& "${GLASSFISH_HOME}\bin\asadmin.bat" set "resources.jdbc-connection-pool.SafiPool.validation-table-name=DUAL"
+& "${GLASSFISH_HOME}\bin\asadmin.bat" set "resources.jdbc-connection-pool.SafiPool.connection-validation-method=table"
+& "${GLASSFISH_HOME}\bin\asadmin.bat" set "resources.jdbc-connection-pool.SafiPool.is-connection-validation-required=true"
+& "${GLASSFISH_HOME}\bin\asadmin.bat" set "resources.jdbc-connection-pool.SafiPool.fail-all-connections=true"
+& "${GLASSFISH_HOME}\bin\asadmin.bat" set "resources.jdbc-connection-pool.SafiPool.statement-cache-type="
+
+& "${GLASSFISH_HOME}\bin\asadmin.bat" create-jdbc-resource --connectionpoolid SafiPool jdbc/safi
+
+# Configure Logging
+& "${GLASSFISH_HOME}\bin\asadmin.bat" set-log-levels "jp.mydns.projectk.safi=FINEST"
+
+# Configure JVM options
+& "${GLASSFISH_HOME}\bin\asadmin.bat" create-jvm-options "-Dsafi.home=$( ${LOCAL_ROOT}.Replace( '\', ',' ).Replace( ':', '\:' ) ),var,safi"
+& "${GLASSFISH_HOME}\bin\asadmin.bat" delete-jvm-options "-Xmx512m"
+& "${GLASSFISH_HOME}\bin\asadmin.bat" create-jvm-options "-Xmx256m"
+
+# Configura deployment
+& "${GLASSFISH_HOME}\bin\asadmin.bat" set "server.admin-service.das-config.autodeploy-enabled=false"
+& "${GLASSFISH_HOME}\bin\asadmin.bat" set "server.admin-service.das-config.dynamic-reload-enabled=false"
+
+
+# Configure console log-level
+Rename-Item -Path "${GLASSFISH_HOME}\glassfish\domains\domain1\config\logging.properties" -NewName "logging.properties.origin"
+
+Get-Content "${GLASSFISH_HOME}\glassfish\domains\domain1\config\logging.properties.origin" `
+| ForEach-Object { $_ -replace `
+  'org.glassfish.main.jul.handler.SimpleLogHandler.level=INFO', `
+  'org.glassfish.main.jul.handler.SimpleLogHandler.level=ALL' } `
+| Set-Content -Path "${GLASSFISH_HOME}\glassfish\domains\domain1\config\logging.properties"
+
+
+# Restart service
+Restart-Service -Name "${SVC_NAME}"
+
+
+Read-Host 'Press enter to exit'
+
+# SIG # Begin signature block
+# MIIGXAYJKoZIhvcNAQcCoIIGTTCCBkkCAQExCzAJBgUrDgMCGgUAMGkGCisGAQQB
+# gjcCAQSgWzBZMDQGCisGAQQBgjcCAR4wJgIDAQAABBAfzDtgWUsITrck0sYpfvNR
+# AgEAAgEAAgEAAgEAAgEAMCEwCQYFKw4DAhoFAAQUASFXDfpaBNCErf1JmGd+S3YJ
+# 5tqgggPPMIIDyzCCArOgAwIBAgIBBjANBgkqhkiG9w0BAQsFADBRMQswCQYDVQQG
+# EwJKUDEOMAwGA1UECAwFT3Nha2ExEjAQBgNVBAoMCVByb2plY3QtSzEeMBwGA1UE
+# AwwVY2EucHJvamVjdC1rLm15ZG5zLmpwMB4XDTI1MDEwMjEzNDcxN1oXDTI3MDky
+# OTEzNDcxN1owYzELMAkGA1UEBhMCSlAxDjAMBgNVBAgMBU9zYWthMRIwEAYDVQQK
+# DAlQcm9qZWN0LUsxDTALBgNVBAMMBHJpcnUxITAfBgkqhkiG9w0BCQEWEnJpcnU0
+# MjkzQGdtYWlsLmNvbTCCASIwDQYJKoZIhvcNAQEBBQADggEPADCCAQoCggEBAOIR
+# PFXMzPTz21pfPjUIs2PJB1WQGDr4+dCCk3j17EFQKx3pZJObLXBtdjAwbqxZJnF/
+# DoyUpsCcVHLwqI7t7+lfsUiq7/MTRFIhJBve5oKRCGK3fYDe2+XS1fY5yFjJmWmm
+# 8eRI7gSJHi9IKCJRzZ3aZ59EmyI5YhefnstYzKh5ro/FtHF8JgJNix5lWRQfsqo8
+# TbJcLNg6nYGmBuKsTeggVmNYi5SS1z662y69kaxX/hWanQCT8bs7PwBKfxNwjOVD
+# C7XVd+lGB51eCfUdLOqgvm5OfKnYw8hb8Yac+NBH82jJGkZD/cdxovT+YGojXUUX
+# IWjX67+Ux25/TYKnFGUCAwEAAaOBmzCBmDAdBgNVHQ4EFgQU1vJRZwN7hBv36Z8K
+# kqO2qHYYMiMwCQYDVR0TBAIwADALBgNVHQ8EBAMCB4AwKwYDVR0lBCQwIgYIKwYB
+# BQUHAwMGCisGAQQBgjcCARUGCisGAQQBgjcCARYwEQYJYIZIAYb4QgEBBAQDAgSw
+# MB8GA1UdIwQYMBaAFLACqQ8B931Vg2H0tDbKTgnZ10bqMA0GCSqGSIb3DQEBCwUA
+# A4IBAQBgBIWW+P8OYjQqQCxauzqKZhALGNAT6mwIcsX7NIitJ7fYA1gH5JeDBCH4
+# GbcehR5sXA7r75xwhGJ7RcH2Lu1jQE/WjBCS1IhZQECP8zPnH27EZ1kYQZZRap30
+# jJU8XgYeyLeiifnRHx+uTplE+zsMp4hZlrBWVdnQBZRwu8FZPPfq9wIYjhe3sX8g
+# g1MIlW5nwzBwxA6tAo8S7/OWsMKA2kwxR2GebGpg5cYyAfjyCLb9d4T7eMwzBnbe
+# 876hMv536wdOnelAN4PmUSORV2BaahYODLaA/Ko4W2PgQuL7CF5WMyRy2mMyogT6
+# lw82ZMtMH6KAu9RfPECBfaYzKBJvMYIB9zCCAfMCAQEwVjBRMQswCQYDVQQGEwJK
+# UDEOMAwGA1UECAwFT3Nha2ExEjAQBgNVBAoMCVByb2plY3QtSzEeMBwGA1UEAwwV
+# Y2EucHJvamVjdC1rLm15ZG5zLmpwAgEGMAkGBSsOAwIaBQCgeDAYBgorBgEEAYI3
+# AgEMMQowCKACgAChAoAAMBkGCSqGSIb3DQEJAzEMBgorBgEEAYI3AgEEMBwGCisG
+# AQQBgjcCAQsxDjAMBgorBgEEAYI3AgEWMCMGCSqGSIb3DQEJBDEWBBRz+4jESw70
+# LWCbNo+8ggD4qxj7ljANBgkqhkiG9w0BAQEFAASCAQCY7FPg6MlAynKteilU4XUi
+# 3vMxCNx24LkH+9MaOjh90VY9XqsxHXz+p2tPvR93jZ9QETS+ooF6O3C9A6Pz06kr
+# mQyTqvSm4fKa6gfMrzXfLqg02jkuy54wCvNnRzeHNZ5KpFfxc4U40ZuzqwxMKQ2w
+# 2gtaP0zPla5418axlcfHaQPjrNCxODUXJTePj2G+4rBFL0G+ge5GphdaJbc68XEo
+# wzGWjygdeiwjp6U80pF6xQ9DS1ZVdD90exvv3QdsSuMDA1u7SwqrYoY8gGATd06x
+# YTs8lvBb1lI6ctmv1jnwxDSu2YUZA66q0hWT+QieYa09sU+V3UjVhAhVtIGPJ8nG
+# SIG # End signature block
