44c8f04dced380dd3841707a610c66cf9c4370f4
riru <riru4293@gmail.com>
2026-03-17 01:06:27 +0900

    Update setup scripts
    CRLF to LF
    - glassfish
    - openssl
    
    OpenSSL 3.6.1

--- setup/OpenSSL/install.ps1
+++ setup/OpenSSL/install.ps1
@@ -1,19 +1,16 @@
 #Requires -Version 7
 ${ErrorActionPreference} = 'Continue'
 
+${LOCAL_ROOT} = "${env:USERPROFILE}\local"
+
 # OpenSSL configuration
-${VER} = '3.6.0'
-${PREFIX} = "${env:USERPROFILE}\local\opt\OpenSSL"
-${DEST} = "${PREFIX}\${VER}"
-
-
 # See: https://kb.firedaemon.com/support/solutions/articles/4000121705
+${VER} = '3.6.1'
 ${SRC} = "https://download.firedaemon.com/FireDaemon-OpenSSL/openssl-${VER}.zip"
 ${ZIP} = "${env:TMP}\$( Split-Path -Path ${SRC} -Leaf )"
-${SRC_HASH} = 'C1C831E8BCCE7D6C204D6813AAFB87C0D44DD88841AB31105185B55CDEC1D759'.ToUpper()
-${HASH_ALG} = 'SHA256' # [MD5|SHA1|SHA256|SHA512]
 
-
+${PREFIX} = "${LOCAL_ROOT}\opt\OpenSSL"
+${DEST} = "${PREFIX}\${VER}"
 
 :transaction do {
 
@@ -21,20 +18,18 @@ ${HASH_ALG} = 'SHA256' # [MD5|SHA1|SHA256|SHA512]
     ${ans} = Read-Host 'Continue? (Y/N)'
     if ( -not ( ( ${ans} -eq 'Y' ) -or ( ${ans} -eq 'y' ) ) ) { Write-Host 'Aborted.'; break :transaction }
 
-    # Confirm require fetch source
-    ${needFetch} = ( -not ( Test-Path "${ZIP}" ) ) -or `
-        ( "${SRC_HASH}" -ne ( Get-FileHash -Path "${ZIP}" -Algorithm "${HASH_ALG}" ).Hash )
-
     # Fetch source if require
-    if( ${needFetch} ) {
-        Write-Host "Downloading to ${ZIP} from ${SRC}"
-        Invoke-WebRequest -Uri "${SRC}" -outfile "${ZIP}"
-    } else {
-        Write-Host 'Skip download OpenSSL. Because already exists.'
-    }
+    Write-Host '-----'
+    Write-Host 'Download OpenSSL'
+    Write-Host "source: ${SRC}"
+    Write-Host "destination ${ZIP}"
+    Invoke-WebRequest -Uri "${SRC}" -outfile "${ZIP}"
 
     # Extract OpenSSL
-    Write-Host "Extracting to ${DEST} from ${ZIP}"
+    Write-Host '-----'
+    Write-Host 'Extract OpenSSL'
+    Write-Output "source: ${ZIP}"
+    Write-Output "destination: ${DEST}"
     if( Test-Path "${DEST}" ) { Remove-Item "${DEST}" -Recurse -Force }
     Expand-Archive -Path "${ZIP}" -DestinationPath "${DEST}"
 
@@ -47,41 +42,41 @@ ${HASH_ALG} = 'SHA256' # [MD5|SHA1|SHA256|SHA512]
 
 } while ( $false )
 
-Read-Host "Press enter to exit."
-
+Read-Host "Press enter to exit.
 # SIG # Begin signature block
-# MIIGXAYJKoZIhvcNAQcCoIIGTTCCBkkCAQExCzAJBgUrDgMCGgUAMGkGCisGAQQB
-# gjcCAQSgWzBZMDQGCisGAQQBgjcCAR4wJgIDAQAABBAfzDtgWUsITrck0sYpfvNR
-# AgEAAgEAAgEAAgEAAgEAMCEwCQYFKw4DAhoFAAQUW6gRvmqOS8ViekuAPV3k3jii
-# euSgggPPMIIDyzCCArOgAwIBAgIBBjANBgkqhkiG9w0BAQsFADBRMQswCQYDVQQG
-# EwJKUDEOMAwGA1UECAwFT3Nha2ExEjAQBgNVBAoMCVByb2plY3QtSzEeMBwGA1UE
-# AwwVY2EucHJvamVjdC1rLm15ZG5zLmpwMB4XDTI1MDEwMjEzNDcxN1oXDTI3MDky
-# OTEzNDcxN1owYzELMAkGA1UEBhMCSlAxDjAMBgNVBAgMBU9zYWthMRIwEAYDVQQK
-# DAlQcm9qZWN0LUsxDTALBgNVBAMMBHJpcnUxITAfBgkqhkiG9w0BCQEWEnJpcnU0
-# MjkzQGdtYWlsLmNvbTCCASIwDQYJKoZIhvcNAQEBBQADggEPADCCAQoCggEBAOIR
-# PFXMzPTz21pfPjUIs2PJB1WQGDr4+dCCk3j17EFQKx3pZJObLXBtdjAwbqxZJnF/
-# DoyUpsCcVHLwqI7t7+lfsUiq7/MTRFIhJBve5oKRCGK3fYDe2+XS1fY5yFjJmWmm
-# 8eRI7gSJHi9IKCJRzZ3aZ59EmyI5YhefnstYzKh5ro/FtHF8JgJNix5lWRQfsqo8
-# TbJcLNg6nYGmBuKsTeggVmNYi5SS1z662y69kaxX/hWanQCT8bs7PwBKfxNwjOVD
-# C7XVd+lGB51eCfUdLOqgvm5OfKnYw8hb8Yac+NBH82jJGkZD/cdxovT+YGojXUUX
-# IWjX67+Ux25/TYKnFGUCAwEAAaOBmzCBmDAdBgNVHQ4EFgQU1vJRZwN7hBv36Z8K
-# kqO2qHYYMiMwCQYDVR0TBAIwADALBgNVHQ8EBAMCB4AwKwYDVR0lBCQwIgYIKwYB
-# BQUHAwMGCisGAQQBgjcCARUGCisGAQQBgjcCARYwEQYJYIZIAYb4QgEBBAQDAgSw
-# MB8GA1UdIwQYMBaAFLACqQ8B931Vg2H0tDbKTgnZ10bqMA0GCSqGSIb3DQEBCwUA
-# A4IBAQBgBIWW+P8OYjQqQCxauzqKZhALGNAT6mwIcsX7NIitJ7fYA1gH5JeDBCH4
-# GbcehR5sXA7r75xwhGJ7RcH2Lu1jQE/WjBCS1IhZQECP8zPnH27EZ1kYQZZRap30
-# jJU8XgYeyLeiifnRHx+uTplE+zsMp4hZlrBWVdnQBZRwu8FZPPfq9wIYjhe3sX8g
-# g1MIlW5nwzBwxA6tAo8S7/OWsMKA2kwxR2GebGpg5cYyAfjyCLb9d4T7eMwzBnbe
-# 876hMv536wdOnelAN4PmUSORV2BaahYODLaA/Ko4W2PgQuL7CF5WMyRy2mMyogT6
-# lw82ZMtMH6KAu9RfPECBfaYzKBJvMYIB9zCCAfMCAQEwVjBRMQswCQYDVQQGEwJK
-# UDEOMAwGA1UECAwFT3Nha2ExEjAQBgNVBAoMCVByb2plY3QtSzEeMBwGA1UEAwwV
-# Y2EucHJvamVjdC1rLm15ZG5zLmpwAgEGMAkGBSsOAwIaBQCgeDAYBgorBgEEAYI3
-# AgEMMQowCKACgAChAoAAMBkGCSqGSIb3DQEJAzEMBgorBgEEAYI3AgEEMBwGCisG
-# AQQBgjcCAQsxDjAMBgorBgEEAYI3AgEWMCMGCSqGSIb3DQEJBDEWBBS227oXyB2L
-# 8LUfMzuOTrPa+VTWkDANBgkqhkiG9w0BAQEFAASCAQC0jlDV8xidZXdjduYNv5pE
-# BFsfw5kVsuee8CipsjUk1o8A/4ld2yKVYPuVdgXOLby5pmLspA6hwszLlSuSYmaF
-# +bTLEiLTD/Sz83FB6Es1jCo8U3hqPbf9mXIZI5qs73qZCIJ19+Z/QGA79iB4a+tC
-# r7UDZnGpvZMBVKeGqS7AOZuOL+Sr2wLXyJ0nYOgCpywwp9KlfGTaCRIcFEEYv2Ci
-# mzqhE0JKt616zHRlduAifN1wXdudly0khY5ewNyJBfBPvVmqmEOB9CBd9he2X7xI
-# //vEvsX13aZca38qsAGgWSHEQMFf7LsXl5iXbrcvh/1fxnnnsnPrxMLT828URFWB
+# MIIGgQYJKoZIhvcNAQcCoIIGcjCCBm4CAQExDzANBglghkgBZQMEAgEFADB5Bgor
+# BgEEAYI3AgEEoGswaTA0BgorBgEEAYI3AgEeMCYCAwEAAAQQH8w7YFlLCE63JNLG
+# KX7zUQIBAAIBAAIBAAIBAAIBADAxMA0GCWCGSAFlAwQCAQUABCC4VSQ0mNpqo5eD
+# OsD/UD8TR1WzDbe77kCfoMlFjH8FDaCCA88wggPLMIICs6ADAgECAgEGMA0GCSqG
+# SIb3DQEBCwUAMFExCzAJBgNVBAYTAkpQMQ4wDAYDVQQIDAVPc2FrYTESMBAGA1UE
+# CgwJUHJvamVjdC1LMR4wHAYDVQQDDBVjYS5wcm9qZWN0LWsubXlkbnMuanAwHhcN
+# MjUwMTAyMTM0NzE3WhcNMjcwOTI5MTM0NzE3WjBjMQswCQYDVQQGEwJKUDEOMAwG
+# A1UECAwFT3Nha2ExEjAQBgNVBAoMCVByb2plY3QtSzENMAsGA1UEAwwEcmlydTEh
+# MB8GCSqGSIb3DQEJARYScmlydTQyOTNAZ21haWwuY29tMIIBIjANBgkqhkiG9w0B
+# AQEFAAOCAQ8AMIIBCgKCAQEA4hE8VczM9PPbWl8+NQizY8kHVZAYOvj50IKTePXs
+# QVArHelkk5stcG12MDBurFkmcX8OjJSmwJxUcvCoju3v6V+xSKrv8xNEUiEkG97m
+# gpEIYrd9gN7b5dLV9jnIWMmZaabx5EjuBIkeL0goIlHNndpnn0SbIjliF5+ey1jM
+# qHmuj8W0cXwmAk2LHmVZFB+yqjxNslws2DqdgaYG4qxN6CBWY1iLlJLXPrrbLr2R
+# rFf+FZqdAJPxuzs/AEp/E3CM5UMLtdV36UYHnV4J9R0s6qC+bk58qdjDyFvxhpz4
+# 0EfzaMkaRkP9x3Gi9P5gaiNdRRchaNfrv5THbn9NgqcUZQIDAQABo4GbMIGYMB0G
+# A1UdDgQWBBTW8lFnA3uEG/fpnwqSo7aodhgyIzAJBgNVHRMEAjAAMAsGA1UdDwQE
+# AwIHgDArBgNVHSUEJDAiBggrBgEFBQcDAwYKKwYBBAGCNwIBFQYKKwYBBAGCNwIB
+# FjARBglghkgBhvhCAQEEBAMCBLAwHwYDVR0jBBgwFoAUsAKpDwH3fVWDYfS0NspO
+# CdnXRuowDQYJKoZIhvcNAQELBQADggEBAGAEhZb4/w5iNCpALFq7OopmEAsY0BPq
+# bAhyxfs0iK0nt9gDWAfkl4MEIfgZtx6FHmxcDuvvnHCEYntFwfYu7WNAT9aMEJLU
+# iFlAQI/zM+cfbsRnWRhBllFqnfSMlTxeBh7It6KJ+dEfH65OmUT7OwyniFmWsFZV
+# 2dAFlHC7wVk89+r3AhiOF7exfyCDUwiVbmfDMHDEDq0CjxLv85awwoDaTDFHYZ5s
+# amDlxjIB+PIItv13hPt4zDMGdt7zvqEy/nfrB06d6UA3g+ZRI5FXYFpqFg4MtoD8
+# qjhbY+BC4vsIXlYzJHLaYzKiBPqXDzZky0wfooC71F88QIF9pjMoEm8xggIIMIIC
+# BAIBATBWMFExCzAJBgNVBAYTAkpQMQ4wDAYDVQQIDAVPc2FrYTESMBAGA1UECgwJ
+# UHJvamVjdC1LMR4wHAYDVQQDDBVjYS5wcm9qZWN0LWsubXlkbnMuanACAQYwDQYJ
+# YIZIAWUDBAIBBQCggYQwGAYKKwYBBAGCNwIBDDEKMAigAoAAoQKAADAZBgkqhkiG
+# 9w0BCQMxDAYKKwYBBAGCNwIBBDAcBgorBgEEAYI3AgELMQ4wDAYKKwYBBAGCNwIB
+# FjAvBgkqhkiG9w0BCQQxIgQgUIUR273jk1wvcVDHl7l2lDFyP+VCZoa0NYcOWUZ9
+# mDcwDQYJKoZIhvcNAQEBBQAEggEAlre5gpcJYKCHDdcbfw/5QZq4pRb1D2taIRKi
+# 1CWPqUifU2cfgba8yyR0MFGwmmO7z4mtFwa+8voUsbTvVf5pdVPz2xHkJpES5qsa
+# HVNO6gYlvSidc7vssEYcxurhZsMfo8LwPScMpo/JZuMbAFyjbn27eZ68OvuE6t4P
+# ZdrQVLrzyeaboCfSHUgiNDoExJOJoS1nmgiEaokblm1+DdTVQUfQglXXCaGlE7Wl
+# qskOi0wIdvQZBw+ulCiyYs1OYP1LD4Yg2xQZviqXRFtQFJpH1jhPYIn62XCfia5b
+# /xoXJgHGRWohj4UvFVgdcGx6xLjDXZpBKuN5i8JzFw8FNQcHQA==
 # SIG # End signature block
