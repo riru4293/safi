81412d6fcc4c3cac86f328cdb5e1814f1aa95f2a
riru <riru4293@gmail.com>
2026-05-07 23:30:39 +0900

    update package-info for 'producer'

--- src/main/java/jp/mydns/projectk/safi/producer/package-info.java
+++ src/main/java/jp/mydns/projectk/safi/producer/package-info.java
@@ -1,6 +1,9 @@
 // Copyright 2025, Project-K
 // SPDX-License-Identifier: BSD-2-Clause
 /**
- <i>Jakarta CDI</i> producer.
+ * Provides <i>Jakarta CDI</i> producer implementations for application-specific beans.
+ *
+ * <p>This package centralizes the creation and exposure of those beans so they can be
+ * provided consistently from a single place throughout the application.
  */
 package jp.mydns.projectk.safi.producer;
