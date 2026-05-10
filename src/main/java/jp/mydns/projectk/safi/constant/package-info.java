82f77005cec58c234dc49e1c20503fa0253dd56a
riru <riru4293@gmail.com>
2026-05-10 18:51:02 +0900

    constantパッケージのJavaDocにproducerパッケージへの改善をフィードバックした。 名詞から、何をするものかの説明に変わっている。

--- src/main/java/jp/mydns/projectk/safi/constant/package-info.java
+++ src/main/java/jp/mydns/projectk/safi/constant/package-info.java
@@ -1,6 +1,13 @@
 // Copyright 2025, Project-K
 // SPDX-License-Identifier: BSD-2-Clause
 /**
- * Constant value.
+ * Provides constant values.
+ * 
+ * <p>
+ * This package contains constant values used across the application. These constants
+ * are intended for use in multiple modules, so they are defined here to avoid duplication.
+ * <p>
+ * Constants that are scoped to a module are not handled here;
+ * such constants should be defined within their respective modules.
  */
 package jp.mydns.projectk.safi.constant;
